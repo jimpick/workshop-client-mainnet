b0776863170832d809d2eda5d5af496bc403aa6f
Jim Pick <jim@protocol.ai>
2020-05-16 21:25:17 -0700

    Finish scan of all miners

---
+++ src/lib/baidu-cities.js
@@ -0,0 +1,34 @@
+const baiduCities = {
+  7: 'Guangdong', // 广东省
+  25: 'Hebei', // 河北省
+  27: 'Shaanxi', // 陕西省
+  28: 'Yunnan', // 云南省
+  29: 'Zhejiang', // 浙江省
+  32: 'Sichuan', // 四川省
+  75: 'Chengdu', // 成都市
+  127: 'Hefei', // 合肥市
+  131: 'Beijing', // 北京市
+  132: 'Chongqing', // 重庆市
+  142: 'Guilin', // 桂林市
+  146: 'Guiyang', // 贵阳市
+  158: 'Changsha', // 长沙市
+  176: 'Taiyuan', // 太原市
+  179: 'Hangzhou', // 杭州市
+  180: 'Ningbo', // 宁波市
+  186: 'Yibin', // 宜宾市
+  187: 'Zhongshan', // 中山市
+  218: 'Wuhan', // 武汉市
+  224: 'Suzhou', // 苏州市
+  257: 'Guangzhou', // 广州市
+  289: 'Shanghai', // 上海市
+  294: 'Huzhou', // 湖州市
+  317: 'Wuxi', // 无锡市
+  331: 'Luzhou', // 泸州市
+  340: 'Shenzhen', // 深圳市
+  363: 'Huaihua', // 怀化市
+  364: 'Shangrao', // 上饶市
+  2912: 'Hong Kong', // 香港特别行政区
+}
+
+
+export default baiduCities
