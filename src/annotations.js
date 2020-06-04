d598289740b630c5d52ebb98aa3fc6387bbc31e2
Jim Pick <jim@backblend.com>
2020-06-04 15:09:56 -0700

    Add annotations for testnet

--- src/annotations.js
+++ src/annotations.js
@@ -1,9 +1,13 @@
 import interopnetAnnotations from './annotations-interopnet'
+import testnetAnnotations from './annotations-testnet'
 import { networkName } from './config'
 
 let annotations = {}
 if (networkName === 'interopnet') {
   annotations = interopnetAnnotations
+}
+if (networkName === 'testnet') {
+  annotations = testnetAnnotations
 }
 
 export default annotations
