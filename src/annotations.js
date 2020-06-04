b44ccb4734a7874ff98416ac87d4c8294a0b6b75
Jim Pick <jim@backblend.com>
2020-06-03 22:31:32 -0700

    Mark active deals

--- src/annotations.js
+++ src/annotations.js
@@ -1,13 +1,13 @@
 const annotations = {
 
-  t01462: 'sealing, China, Zhejiang, power, top miner',
-  t01473: 'sealing, China, Shenzhen, power',
+  t01462: 'active, China, Zhejiang, power, top miner',
+  t01473: 'active, China, Shenzhen, power',
   t01481: 'sealing, China, Shenzhen',
   t01483: 'sealing, China, Zhejiang, power',
   t01502: 'sealing, China, Shenyang, power',
-  t01540: 'sealing, China, Zhejiang, power, top miner',
-  t01554: 'sealing, China, Yunnan, power',
-  t01563: 'sealing, Latvia, Riga, power, top miner',
+  t01540: 'active, China, Zhejiang, power, top miner',
+  t01554: 'active, China, Yunnan, power',
+  t01563: 'active, Latvia, Riga, power, top miner',
   t01570: 'sealing, China, Wuhan, power',
   t01595: 'sealing, China, Shanghai',
   t01608: 'sealing, Poland, Krakow, power, verified',
@@ -19,9 +19,9 @@ const annotations = {
   t01672: 'sealing, China, Hangzhou',
   t01684: 'sealing, 32G, China, Shijiazhuang, power',
   t01686: 'sealing, China, Hangzhou',
-  t01711: 'sealing, China, Yunnan, power',
+  t01711: 'active, China, Yunnan, power',
   t01713: 'sealing, China, Yunnan',
-  t01725: 'sealing, China, Changsha',
+  t01725: 'active, China, Changsha',
   t01730: 'sealing, China, Shaanxi',
 
   // Non-routables (with power)
