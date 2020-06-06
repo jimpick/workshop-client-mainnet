021f466ddb073bd3e1d765f4419a60f3803d29dd
Jim Pick <jim@backblend.com>
2020-06-06 12:18:05 -0700

    Add miner, links to filscan, filfox

--- src/annotations-interopnet.js
+++ src/annotations-interopnet.js
@@ -1,12 +1,13 @@
 const annotations = {
 
   // Routables
-  t01536: 'new, China, Zhejiang, power, top miner',
-  t01540: 'new, China, Zhejiang, power, top miner',
+  t01536: 'active, China, Zhejiang, power, top miner',
+  t01540: 'active, China, Zhejiang, power, top miner',
   t01534: 'new, China, Shijiazhuang',
   t01571: 'new, China, Zhejiang',
   t01573: 'new, China, Sichuan',
   t01581: 'new, China, Shaanxi',
+  t01597: 'new, China, Changsha',
 
   // Non-routables (with power)
   t01075: 'NR, power',
