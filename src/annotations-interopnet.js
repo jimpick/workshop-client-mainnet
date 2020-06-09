1df937ef1a455a27d26b3abb92e9bcc1f6c3dc31
Jim Pick <jim@protocol.ai>
2020-06-08 18:03:43 -0700

    Update annotations

--- src/annotations-interopnet.js
+++ src/annotations-interopnet.js
@@ -1,12 +1,15 @@
 const annotations = {
 
   // Routables
-  t01803: 'new, Jim',
+  t01803: 'USA, Boardman, Jim',
 
   // Non-routables (with power)
+  t1805: 'NR, power',
+  t1807: 'NR, power',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
+  t01002: 'NR - bootstrap',
 }
 
 export default annotations
