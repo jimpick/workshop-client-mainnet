c674ed7495720c02afb5589a5415f7448cc5340e
Jim Pick <jim@protocol.ai>
2020-06-10 20:51:43 -0700

    Update annotations

--- src/annotations-interopnet.js
+++ src/annotations-interopnet.js
@@ -1,39 +1,40 @@
 const annotations = {
 
   // Routables
-  t01803: 'active, USA, Boardman, AWS - Jim',
+  t01803: 'active, USA, Boardman, power, AWS - Jim',
   t01824: 'active, China, Yangzhou',
-  t01826: 'sealing, power - China, Shaanxi',
-  t01841: 'active, China, Zhejiang, verified',
+  t01826: 'sealing, China, Shaanxi',
+  t01841: 'active, China, Zhejiang, power, verified',
   t01843: 'new, China, Changsha, power',
-  t01845: 'sealing, China, Zhejiang, top miner, verified',
+  t01845: 'sealing, China, Zhejiang, power, top miner, verified',
   t01847: 'new, China, Guilin',
-  t01856: 'error, China, Changsha', // routing not found
   t01904: 'sealing, China, Shenyang',
-  t01910: 'error, China, Changsha, 32G', // routing not found
+  t01906: 'new, China, Shijiazhuang, power',
   t01916: 'sealing, China, Shenzhen',
-  t01928: 'sealing, China, Dongguan',
   t02023: 'sealing, China, Yunnan',
   t02026: 'sealing, China, Shaanxi',
-  t02036: 'sealing, China, Yunnan',
-  t02051: 'active, China, Foshan',
+  t02036: 'sealing, China, Yunnan, power, verified',
+  t02051: 'active, China, Foshan, power',
   t02067: 'sealing, USA, Los Angeles',
-  t02092: 'sealing, China, Dongguan',
-  t02094: 'sealing, China, Zhejiang',
-  t02098: 'sealing, China, Wuhan',
-  t02112: 'error, China, Yunnan', // routing not found
+  t02092: 'sealing, China, Dongguan, power',
+  t02094: 'sealing, China, Zhejiang, power, 32G',
+  t02098: 'sealing, China, Wuhan, power, 32G',
   t02114: 'sealing, China, Shaanxi',
   t02147: 'sealing, China, Nanning',
-  t02162: 'active, China, Shenyang',
-  t02209: 'new, China, Hangzhou',
+  t02162: 'active, China, Shenyang, power, verified',
+  t02209: 'new, China, Hangzhou, power',
 
   // Non-routables (with power)
   t01805: 'NR, power',
   t01807: 'NR, power',
   t01816: 'NR, power',
   t01818: 'NR, power',
-  t01890: 'NR, power - China, Shaanxi', // routing not found
+  t01856: 'NR - China, Changsha',
+  t01890: 'NR, power - China, Shaanxi',
+  t01910: 'NR, power - China, Changsha, 32G',
+  t01928: 'NR, power - China, Dongguan',
   t02104: 'NR, power',
+  t02112: 'NR, power - China, Yunnan',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
