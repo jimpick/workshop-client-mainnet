a10bd75a82d7a0ec9e83e2e7348323fc9350191e
Jim Pick <jim@backblend.com>
2020-06-17 11:45:12 -0700

    New miners, annotations

--- src/annotations-interopnet.js
+++ src/annotations-interopnet.js
@@ -2,17 +2,21 @@ const annotations = {
 
   // Routables
   t01813: 'active, USA, Boardman, power, Jim',
-  t01820: 'sealing, China, Sichuan, 32G',
-  t01850: 'sealing, China, Hong Kong, 32G',
-  t01889: 'sealing, China, Foshan',
-  t01929: 'sealing, China, Zhejiang',
-  t01972: 'sealing, China, Shanghai',
-  t02000: 'sealing, China, Guangdong',
+  t01820: 'sealing, China, Sichuan, 32G, power, top miner',
+  t01889: 'active, China, Foshan, power, verified',
+  t01929: 'active, China, Zhejiang, power, verified',
+  t01972: 'active, China, Shanghai, power',
+  t02000: 'active, China, Guangdong, power, verified',
+  t02020: 'sealing, China, Changsha, 32G',
+  t02041: 'sealing, China, Chengdu, power, verified',
+  t02053: 'sealing, China, Shenzhen, power',
+  t02192: 'sealing, China, Guilin',
 
   // Non-routables (with power)
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
+  t01850: 'NR - China, Hong Kong, 32G, top miner',
 
 }
 
