8ec9d07ab1bb20e06bd146ba5c7f0ad79d5d8426
Jim Pick <jim@backblend.com>
2020-06-16 18:30:27 -0700

    Update for new interopnet

--- src/annotations-interopnet.js
+++ src/annotations-interopnet.js
@@ -1,57 +1,13 @@
 const annotations = {
 
   // Routables
-  t01803: 'active, USA, Boardman, AWS - Jim',
-  t01812: 'sealing, China, Sichuan, power, top miner',
-  t01824: 'active, China, Yangzhou',
-  t01841: 'active, China, Zhejiang, verified',
-  t01845: 'sealing, China, Zhejiang, top miner, verified',
-  t01904: 'sealing, China, Shenyang',
-  t01906: 'validating, China, Shijiazhuang, power, top miner, 32G',
-  t02023: 'sealing, China, Yunnan',
-  t02026: 'active, China, Shaanxi',
-  t02036: 'active, China, Yunnan, power, verified',
-  t02051: 'active, China, Foshan, power',
-  t02067: 'sealing, USA, Los Angeles',
-  t02092: 'sealing, China, Dongguan',
-  t02094: 'active, China, Zhejiang',
-  t02114: 'sealing, China, Shaanxi',
-  t02162: 'active, China, Shenyang',
-  t02318: 'sealing, China, Shenzhen, power, 32G',
-  t02339: 'sealing, China, Ningbo, 32G',
-  t02405: 'sealing, China, Weifang, 32G',
-  t02411: 'active, China, Changsha, power',
-  t02418: 'sealing, China, Shaanxi',
-  t02430: 'active, China, Yunnan, power',
-  t02443: 'active, China, Yunnan, power',
-  t02541: 'active, China, Ningbo, power, top miner',
-  t02544: 'sealing, China, Guilin, power',
-  t02687: 'sealing, China, Fuzhou, 32G',
-  t02817: 'sealing, China, Fuzhou, 32G',
-  t02849: 'sealing, China, Changsha, 32G',
+  t01813: 'new, USA, Boardman, Jim',
 
   // Non-routables (with power)
-  t01805: 'NR, power',
-  t01807: 'NR, power',
-  t01816: 'NR, power',
-  t01818: 'NR, power',
-  t01826: 'NR - China, Shaanxi',
-  t01843: 'NR - China, Changsha, power',
-  t01856: 'NR - China, Changsha',
-  t01886: 'NR - China, Guangdong, power',
-  t01890: 'NR, power - China, Shaanxi',
-  t01910: 'NR - China, Changsha, 32G',
-  t01916: 'NR - China, Shenzhen',
-  t01928: 'NR, power - China, Dongguan',
-  t02098: 'NR - China, Wuhan',
-  t02104: 'NR, power',
-  t02112: 'NR - China, Yunnan',
-  t02147: 'NR - China, Nanning',
-  t02209: 'NR, power - China, Hangzhou',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
-  t01002: 'NR - bootstrap',
+
 }
 
 export default annotations
