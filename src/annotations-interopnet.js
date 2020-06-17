e7f7e688dacf8e82ba94ae00c210aee225cd0a85
Jim Pick <jim@backblend.com>
2020-06-16 23:19:32 -0700

    More miners

--- src/annotations-interopnet.js
+++ src/annotations-interopnet.js
@@ -4,6 +4,10 @@ const annotations = {
   t01813: 'active, USA, Boardman, power, Jim',
   t01820: 'sealing, China, Sichuan, 32G',
   t01850: 'sealing, China, Hong Kong, 32G',
+  t01889: 'sealing, China, Foshan',
+  t01929: 'sealing, China, Zhejiang',
+  t01972: 'sealing, China, Shanghai',
+  t02000: 'sealing, China, Guangdong',
 
   // Non-routables (with power)
 
