c01444eba363aed723213efb3e9432d763eeee97
Jim Pick <jim@protocol.ai>
2020-06-19 18:03:05 -0700

    Update annotations

--- src/annotations-testnet.js
+++ src/annotations-testnet.js
@@ -5,29 +5,34 @@ const annotations = {
   t01215: 'sealing, China, Zhongshan',
   t01459: 'sealing, China, Guilin',
   t01784: 'sealing, China, Shenzhen',
-  t01985: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
   t02020: 'active, China, Chengdu',
   t04463: 'sealing, China, Dongguan',
   t04842: 'sealing, China, Changsha',
   t05900: 'active, China, Changzhou',
-  t07211: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
   t08293: 'sealing, China, Shangrao',
-  t08459: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
   t09833: 'sealing, China, Mianyang',
-  t013836: 'new, China, Guangdong',
+  t013836: 'sealing, China, Guangdong',
   t013966: 'sealing, China, Guangdong',
   t020889: 'sealing, China, Shenzhen',
   t020894: 'sealing, China, Xiamen',
   t029571: 'sealing, Korea, Seongnam-si',
   t033740: 'sealing, China, Ningbo',
-  t039025: 'new, USA, San Francisco',
+  t039025: 'sealing, USA, San Francisco',
   t040585: 'sealing, China, Shanghai',
   t052328: 'sealing, China, Beijing',
-  t055393: 'new, China, Beijing',
-  t056726: 'new, China, Hunan',
-  t091046: 'new, Singapore',
-  t0102008: 'new, China, Wuxi',
-  t0106575: 'new, China, Beijing',
+  t056726: 'sealing, China, Hunan',
+  t0106575: 'sealing, China, Beijing',
+  t0106603: 'sealing, Germany',
+
+  // Error
+  t07211: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
+  t01985: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
+  t08459: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
+  t0102008: 'error, China, Wuxi', // unexpected deal status while waiting for data request: 7
+  
+  // Non-routable
+  t055393: 'NR - China, Beijing',
+  t091046: 'NR - Singapore',
 
   // Bootstrappers
   t01000: 'bootstrap, NR',
