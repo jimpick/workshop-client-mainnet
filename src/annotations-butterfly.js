bb7db5bb2fbb2370f8fb26514ab27786978db4c8
Jim Pick <jim@protocol.ai>
2020-07-11 10:11:05 -0700

    Update list of miners

--- src/annotations-butterfly.js
+++ src/annotations-butterfly.js
@@ -1,12 +1,14 @@
 const annotations = {
   // Active
-  t01012: 'active, China, Guangdong, 1h',
-  t01025: 'active, Poland, Krakow, @magik6k, 1h',
-  t01027: 'active, China, Foshan, 1h',
-  t01005: 'active, China, Guilin, 36m',
+  t01012: 'active, China, Guangdong, 1h, 1h',
+  t01025: 'active, Poland, Krakow, @magik6k, 1h, 1h',
+  t01027: 'active, China, Foshan, 1h, 1h',
+  t01005: 'active, China, Guilin, 36m, 42m',
 
   // New, sealing, recycle
   t01021: 'sealing, Brazil, São Paulo, 32G, fri',
+  t01119: 'new, China, Guandong, 32G',
+  t01128: 'new, China, Suzhou, 32G',
 
   // Error
 
@@ -14,15 +16,19 @@ const annotations = {
   t01062: "StorageDealCheckForAcceptance, USA, Coeur d'Alene",
 
   // Non-routables (with power)
+  t01003: 'NR, @travisperson',
   t01020: 'NR, power, 32G',
   t01037: 'NR, power',
   t01065: 'NR, power',
   t01067: 'NR, power',
+  t01070: 'NR, power',
+  t01081: 'NR, power',
+  t01103: 'NR, power',
+  t01115: 'NR, power',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
   t01001: 'NR - bootstrap',
-  t01003: 'NR - bootstrap'
 }
 
 export default annotations
