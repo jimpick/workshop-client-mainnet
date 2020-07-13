35424ecbdc234ea8c97da8b146bd28164b5cadde
Jim Pick <jim@backblend.com>
2020-07-13 13:05:26 -0700

    Nerpa active deals

--- src/annotations-nerpa.js
+++ src/annotations-nerpa.js
@@ -1,12 +1,21 @@
 const annotations = {
 
+  // Active
+  t01023: 'active, USA, Seattle, @eshon mining machine, 15m',
+  t01025: 'active, USA, Coeur d\'Alene, 15m',
+  t01029: 'active, USA, Ashburn, 15m',
+  t01031: 'active, USA, Ashburn, 15m',
+
+  // New, Sealing
+
   // Routables
-  t01007: 'new, Germany, Haslach im Kinzigtal',
+  t01007: 'sealing, Germany, Haslach im Kinzigtal, mon',
 
   // More
+
+  // NR
   t01012: '@eshon, development desktop',
-  t01021: '@mburns',
-  t01023: '@eshon mining machine',
+  t01021: 'NR, @mburns',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
