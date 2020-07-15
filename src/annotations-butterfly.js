c7178b6ee5d7581a3c012fd667144f6fa4203ecb
Jim Pick <jim@protocol.ai>
2020-07-15 12:57:57 -0700

    Butterfly only

--- src/annotations-butterfly.js
+++ src/annotations-butterfly.js
@@ -1,22 +1,27 @@
 const annotations = {
   // Active
-  t01025: 'active, Poland, Krakow, @magik6k, 1h, 1h, 1h, 1h, mon',
-  t01135: 'active, Finland, Helsinki, 3h, 1h, 1h, mon',
-  t01154: 'active, Canada, Vancouver, @jimpick, 25m, 27m, mon',
-  t09095: 'active, China, Foshan, 1h, mon',
+  t01025: 'active, Poland, Krakow, @magik6k, 1h, 1h, 1h, 1h, 1h',
+  t01135: 'active, Finland, Helsinki, 3h, 1h, 1h, 1h',
+  t01154: 'active, Canada, Vancouver, @jimpick, 25m, 27m, 25m',
+  t09095: 'active, China, Foshan, 1h, 1h',
+  t09146: 'active, USA, Boardman, 1h',
+  t09148: 'active, USA, Boardman, 1h',
 
   // New, sealing, recycle
   t01003: 'sealing, USA, Seattle, @travisperson, 3h, mon',
   t01021: 'sealing, Brazil, São Paulo, 32G, fri, sun',
-  t01062: "sealing, USA, Coeur d'Alene, mon",
   t01119: 'sealing, China, Guandong, 32G, sun',
-  t09114: 'sealing, China, Suzhou, mon',
-  t09126: 'new, China, Chengdu, mon',
+  t09114: 'sealing, China, Suzhou, 32G, mon',
+  t09167: 'new, USA, @travisperson, Seattle',
+  t09266: 'new, China, Chengdu',
 
   // Asks
   t09096: 'minask, China, Foshan, sun', // storage price per epoch less than asking price: 2500 < 953674316406
 
   // Error
+  t01062: "error, USA, Coeur d'Alene", // error in deal activation: handling applied event: deal wasn't active: deal=3390, parentState=bafy2bzacebpfjxdfzxuffwp6ijohzckxoag2k4o6ak6y7p2kwgy7gphjrz3so, h=1201
+  t09126: 'error, China, Chengdu', // sending proposal to storage provider failed: dial backoff
+  t09274: 'error, China, Suzhou', // error reading Response message: stream reset
 
   // Stuck
 
@@ -36,6 +41,7 @@ const annotations = {
   t01133: 'NR, power, 32G',
   t01152: 'NR, power, 32G',
   t01159: 'NR, power',
+  t09144: 'NR, power',
 
   // Non-routables with locations
   t01005: 'NR, China, Guilin, 36m, 42m',
