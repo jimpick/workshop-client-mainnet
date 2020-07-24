b32c5ab62571e55cc4b94e52e87bd33340eef3af
Jim Pick <jim@backblend.com>
2020-07-24 00:06:32 -0700

    New miners

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -5,7 +5,18 @@ const annotations = {
   t01032: 'sealing, China, Hangzhou, 32G, thu',
   t01050: 'sealing, China, Chongqing, 32G, thu',
   t01091: 'sealing, China, Ningbo, 32G, thu',
+  t01158: 'new, China, Chengdu',
+  t01170: 'new, China, Shenzhen',
+  t01199: 'new, China, Guangdong',
+  t01203: 'new, China, Guangzhou',
+  t01207: 'new, Korea, Yangcheon-gu',
+  t01226: 'new, Canada',
+  t01227: 'new, Brazil, Sao Paulo',
+  t01229: 'new, Brazil, Sao Paulo',
   t01235: 'sealing, Canada, Vancouver, 512M, @jimpick, thu',
+  t01258: 'new, China, Shenzhen',
+  t01339: 'new, China, Shanghai',
+  t01348: 'new, China, Hong Kong',
   t01372: 'sealing, China, Shijiazhuang, 512M, thu',
   t01392: 'sealing, Korea, Guro-gu, 512M, thu',
   t01404: 'sealing, China, Wuxi, 32G, thu',
@@ -13,7 +24,13 @@ const annotations = {
   t01417: 'sealing, China, Yangzhou, 32G, thu',
   t01418: 'sealing, China, Shandong, 512M, thu',
   t01434: 'sealing, USA, Boardman, 32G, @jimpick, thu',
+  t01442: 'new, China, Foshan',
   t01461: 'sealing, China, Hangzhou, 512M, thu',
+  t01476: 'new, China, Xianning',
+  t01513: 'new, China, Foshan',
+  t01517: 'new, China, Wuhan',
+  t01595: 'new, China, Beijing',
+  t02141: 'new, China, Shijiazhuang',
 
 
   // Asks
@@ -31,6 +48,7 @@ const annotations = {
   // Bootstrappers
   t01000: 'NR - bootstrap',
   t01001: 'NR - bootstrap',
+  t01002: 'NR - bootstrap',
 }
 
 export default annotations
