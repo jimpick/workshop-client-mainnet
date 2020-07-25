25aacc64f3d3e12cd591ccf2479f7eb40f0617fc
Jim Pick <jim@backblend.com>
2020-07-24 21:40:43 -0700

    Fix shell scripts

--- retrieve-test/get-commands.sh
+++ retrieve-test/get-commands.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cat deals.json | jq -r '.[] | {miner, deal: .deals[]} | "TIMESTAMP=`date +%s` bash -c \"time lotus client retrieve --miner=\(.miner) \(.deal.cid) /home/ubuntu/downloads/\(.miner)-\(.deal.dealId)-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/\(.miner)-\(.deal.dealId)-$TIMESTAMP.log\"\n"'
+cat deals.json | jq -r '.[] | {miner, deal: .deals[]} | "(TIMESTAMP=`date +%s`; /usr/bin/time lotus client retrieve --miner=\(.miner) \(.deal.cid) /home/ubuntu/downloads/\(.miner)-\(.deal.dealId)-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/\(.miner)-\(.deal.dealId)-$TIMESTAMP.log)\n"'
