5ac568832fdec5f8cd328958a71415922be6187c
Jim Pick <jim@backblend.com>
2020-07-24 20:47:11 -0700

    Experiment with retrieval timings using CLI

---
+++ retrieve-test/retrieve.sh
@@ -0,0 +1,42 @@
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01032 bafk2bzacedcwlbkd6gtqub4ctpj6r4piwdjbv7ne4wt7wegyfgr5xxxqfh63i /home/ubuntu/downloads/t01032-2320-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01032-2320-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01091 bafk2bzacedcwlbkd6gtqub4ctpj6r4piwdjbv7ne4wt7wegyfgr5xxxqfh63i /home/ubuntu/downloads/t01091-2323-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01091-2323-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01170 bafk2bzacea2lp3yct2pw55v4m4byhkvia5ilf45bomkiewbn4jdmcke4bptj6 /home/ubuntu/downloads/t01170-7356-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01170-7356-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01203 bafk2bzaceaww6g2sgz45ahjjzooemvtsnrra6c3adjexwl7bh65ybckj532sq /home/ubuntu/downloads/t01203-2678-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01203-2678-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01258 bafk2bzaceaww6g2sgz45ahjjzooemvtsnrra6c3adjexwl7bh65ybckj532sq /home/ubuntu/downloads/t01258-2679-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01258-2679-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01339 bafk2bzaceaww6g2sgz45ahjjzooemvtsnrra6c3adjexwl7bh65ybckj532sq /home/ubuntu/downloads/t01339-2680-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01339-2680-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01407 bafk2bzacea4g7mn3l7vu7nidbqoqv3medwlwqlfe5lf6wqrpemzskefqscv3q /home/ubuntu/downloads/t01407-2523-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01407-2523-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01434 bafk2bzacea4g7mn3l7vu7nidbqoqv3medwlwqlfe5lf6wqrpemzskefqscv3q /home/ubuntu/downloads/t01434-2522-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01434-2522-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01461 bafk2bzacea4g7mn3l7vu7nidbqoqv3medwlwqlfe5lf6wqrpemzskefqscv3q /home/ubuntu/downloads/t01461-2508-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01461-2508-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t01595 bafk2bzaceaww6g2sgz45ahjjzooemvtsnrra6c3adjexwl7bh65ybckj532sq /home/ubuntu/downloads/t01595-2685-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t01595-2685-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t02836 bafk2bzacea2lp3yct2pw55v4m4byhkvia5ilf45bomkiewbn4jdmcke4bptj6 /home/ubuntu/downloads/t02836-7367-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t02836-7367-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t02840 bafk2bzacebrmoxbp6vahdlk6xxykqx5lfphftvjzzoylwff4xmdhbsp5okbp2 /home/ubuntu/downloads/t02840-6397-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t02840-6397-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03211 bafk2bzacebrmoxbp6vahdlk6xxykqx5lfphftvjzzoylwff4xmdhbsp5okbp2 /home/ubuntu/downloads/t03211-6399-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03211-6399-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03211 bafk2bzacea2lp3yct2pw55v4m4byhkvia5ilf45bomkiewbn4jdmcke4bptj6 /home/ubuntu/downloads/t03211-7351-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03211-7351-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03266 bafk2bzacebrmoxbp6vahdlk6xxykqx5lfphftvjzzoylwff4xmdhbsp5okbp2 /home/ubuntu/downloads/t03266-6395-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03266-6395-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03289 bafk2bzacebrmoxbp6vahdlk6xxykqx5lfphftvjzzoylwff4xmdhbsp5okbp2 /home/ubuntu/downloads/t03289-6405-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03289-6405-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03289 bafk2bzacea2lp3yct2pw55v4m4byhkvia5ilf45bomkiewbn4jdmcke4bptj6 /home/ubuntu/downloads/t03289-7353-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03289-7353-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03360 bafk2bzacebrmoxbp6vahdlk6xxykqx5lfphftvjzzoylwff4xmdhbsp5okbp2 /home/ubuntu/downloads/t03360-6409-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03360-6409-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03360 bafk2bzacea2lp3yct2pw55v4m4byhkvia5ilf45bomkiewbn4jdmcke4bptj6 /home/ubuntu/downloads/t03360-7359-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03360-7359-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03361 bafk2bzacebrmoxbp6vahdlk6xxykqx5lfphftvjzzoylwff4xmdhbsp5okbp2 /home/ubuntu/downloads/t03361-6411-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03361-6411-$TIMESTAMP.log"
+
+TIMESTAMP=`date +%s` bash -c "time lotus client retrieve --miner=t03361 bafk2bzacea2lp3yct2pw55v4m4byhkvia5ilf45bomkiewbn4jdmcke4bptj6 /home/ubuntu/downloads/t03361-7360-$TIMESTAMP.jpg 2>&1 | tee /home/ubuntu/downloads/t03361-7360-$TIMESTAMP.log"
+
