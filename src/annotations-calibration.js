7774ca15bfb386f9e3fa81e75365bc087cf5e2dd
Jim Pick <jim@backblend.com>
2020-07-26 12:39:17 -0700

    More updates

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -1,57 +1,67 @@
 const annotations = {
   // Active
-  t01032: 'active, China, Hangzhou, 32G, 11h, fri',
   t01091: 'active, China, Ningbo, 32G, 15h, sat',
-  t01170: 'active, China, Shenzhen, 32G, 8h, fri',
-  t01203: 'active, China, Guangzhou, 32G, 11h, 8h, sat',
-  t01258: 'active, China, Shenzhen, 512M, 3h, fri, sat',
-  t01339: 'active, China, Shanghai, 32G, 11h, fri',
-  t01434: 'active, USA, Boardman, 32G, @jimpick, 16h, sat',
-  t01461: 'active, China, Hangzhou, 512M, 2h, fri, sat',
-  t01595: 'active, China, Beijing, 32G, 13h, sat',
-  t03211: 'active, China, Wuhan, 512M, 2h, 2h, sat',
-  t03266: 'active, China, Shandong, 512M, 2h, fri, sat',
-  t03289: 'active, China, Beijing, 512M, 1h, 2h, sat',
-  t03360: 'active, USA, Boardman, 512M, @ribasushi, 1h, 1h, sat',
-  t03361: 'active, USA, Boardman, 512M, @ribasushi, 1h, 1h, sat',
+  t01170: 'active, China, Shenzhen, 32G, 8h, fri, sun',
+  t01203: 'active, China, Guangzhou, 32G, 11h, 8h, 7h, sun',
+  t01235: 'active, Canada, Vancouver, 512M, @jimpick, 1h, 1h, sun',
+  t01258: 'active, China, Shenzhen, 512M, 3h, 2h, sun',
+  t01417: 'active, China, Yangzhou, 32G, 7h, sun',
+  t01434: 'active, USA, Boardman, 32G, @jimpick, 16h, 16h, sun',
+  t01461: 'active, China, Hangzhou, 512M, 2h, fri, sat, sun',
+  t01595: 'active, China, Beijing, 32G, 13h, 12h, sun',
+  t03038: 'active, Poland, Krakow, 512M, @magik6k, 6h, sun',
+  t03211: 'active, China, Wuhan, 512M, 2h, 2h, sat, sun',
+  t03289: 'active, China, Beijing, 512M, 1h, 2h, sat, sun',
+  t03360: 'active, USA, Boardman, 512M, @ribasushi, 1h, 1h, 1h, sun',
+  t03361: 'active, USA, Boardman, 512M, @ribasushi, 1h, 1h, 1h, sun',
+  t03590: 'active, China, Zhongshan, 512M, 2h, sun',
+  t03646: 'active, Finland, Helsinki, 512M, 2h, sun',
 
   // New, sealing, recycle
   t01050: 'sealing, China, Chongqing, 32G, thu, sat',
-  t01072: 'sealing, USA, Galveston + China, Guangdong, 32G, fri',
+  t01158: 'sealing, China, Chengdu, 32G, sun',
   t01226: 'sealing, Canada, 32G, thu, sat',
   t01227: 'sealing, Brazil, Sao Paulo, 32G, thu, sat',
   t01229: 'sealing, Brazil, Sao Paulo, 32G, thu, sat',
   t01404: 'sealing, China, Wuxi, 32G, thu, sat',
-  t01417: 'sealing, China, Yangzhou, 32G, thu, sat',
   t01442: 'sealing, China, Foshan, 32G, thu, sat',
-  t01517: 'sealing, China, Wuhan, 32G, fri',
-  t02940: 'sealing, China, Zhongshan, 32G, fri',
-  t02970: 'sealing, Korea, Yangcheon-gu, 32G, fri',
-  t03014: 'sealing, Korea, Seoul, 32G, fri',
-  t03038: 'sealing, Poland, Krakow, 512M, @magik6k, fri, sat',
-  t03060: 'sealing, China, Hangzhou, 512M, fri, sat',
-  t03068: 'sealing, China, Shijiazhuang, 32G, fri',
-  t03172: 'sealing, China, Guilin, 32G, fri',
-  t03210: 'sealing, China, Beijing, 512M, fri, sat',
-  t03233: 'sealing, China, Shijiazhuang, 512M, fri, sat',
-  t03316: 'sealing, China, Wuxi, 32G, fri',
-  t03318: 'sealing, China, Wuxi, 32G, fri',
-  t03369: 'sealing, Canada, Stratford, 32G, fri',
+  t01517: 'sealing, China, Wuhan, 32G, fri, sun',
+  t02944: 'sealing, China, Guilin, 512M, fri, sat, sun',
+  t02970: 'sealing, Korea, Yangcheon-gu, 32G, fri, sun',
+  t03014: 'sealing, Korea, Seoul, 32G, fri, sun',
+  t03060: 'sealing, China, Hangzhou, 512M, fri, sat, sun',
+  t03068: 'sealing, China, Shijiazhuang, 32G, fri, sun',
+  t03172: 'sealing, China, Guilin, 32G, fri, sun',
+  t03203: 'sealing, China, Wuhan, 32G, 12h, sun',
+  t03210: 'new, China, Beijing, 512M',
+  t03233: 'sealing, China, Shijiazhuang, 512M, fri, sat, sun',
+  t03316: 'sealing, China, Wuxi, 32G, fri, sun',
+  t03318: 'sealing, China, Wuxi, 32G, fri, sun',
+  t03369: 'sealing, Canada, Stratford, 32G, fri, sun',
   t03396: 'sealing, Sweden, Stockholm, 32G, sat',
-  t03402: 'sealing, China, Shanghai, 32G, fri',
-  t03414: 'sealing, Germany, Frankfurt, 32G, fri',
-  t03438: 'sealing, China, Luzhou, 32G, fri',
+  t03402: 'sealing, China, Shanghai, 32G, fri, sun',
+  t03440: 'sealing, China, Chengdu, 32G, sat, sun',
   t03465: 'sealing, China, Hangzhou, 32G, sat',
   t03500: 'sealing, China, Changsha, 32G, sat',
   t03518: 'sealing, China, Luzhou, 32G, sat',
   t03532: 'sealing, China, Wuxi, 32G, sat',
   t03549: 'sealing, Germany, Frankfurt, 32G, sat',
-  t03550: 'sealing, China, Changsha, 512M, sat',
   t03585: 'sealing, China, Changsha, 32G, sat',
-  t03590: 'sealing, China, Zhongshan, 512M, sat',
   t03591: 'sealing, China, Chengdu, 32G, sat',
   t03608: 'sealing, China, Hanzhou, 32G, sat',
-  t03614: 'sealing, China, Shenzhen, 512M, sat',
+  t03614: 'sealing, China, Shenzhen, 512M, sat, sun',
+  t03637: 'sealing, China, Hong Kong, 512M, sat, sun',
+  t03645: 'sealing, Sweden, Stockholm, 32G, sat',
+  t03692: 'sealing, Russia, Novosibirsk, 32G, sun',
+  t03709: 'sealing, USA, Bellevue, 32G, sun',
+  t03730: 'sealing, Singapore, 32G, sun',
+  t03736: 'sealing, China, Changsha, 512M, sun',
+  t03749: 'sealing, China, Shandong, 512M, sun',
+  t03776: 'sealing, Malaysia, Johor Bahru, 512M, sun',
+  t03779: 'sealing, China, Dongguan, 512M, sun',
+  t03782: 'sealing, China, Jinan, 32G, sun',
+  t03783: 'sealing, China, Shanghai, 32G, sun',
+  t03806: 'new, Korea, Geumcheon-gu, 512M',
 
   // Asks
   t01513: 'minask, China, Foshan, 512M, 2h', // unexpected deal status while waiting for data request: 11 (StorageDealFailing). Provider message: deal rejected: storage price per epoch less than asking price: 2000000 < 610351562500
@@ -60,22 +70,22 @@ const annotations = {
   t01348: 'no-online, China, Hong Kong, 512M', // unexpected deal status while waiting for data request: 11 (StorageDealFailing). Provider message: deal rejected: miner is not considering online storage deals
 
   // Error
+  t01072: 'error, USA, Galveston + China, Guangdong, 32G, fri', // deal failed: (State=26) handing off deal to node: AddPiece failed: adding piece to sector: writing piece: RPC error (-32700): unmarshaling params for 'Filecoin.AddPiece' (param: *io.Reader): json: cannot unmarshal object into Go value of type io.Reader
   t01199: 'error, China, Guangdong, 32G, thu', // deal failed: (State=26) deal data verification failed: error generating CommP: failed to build tree Caused by: 0: failed to create data store 1: No such file or directory (os error 2)
-  t01158: 'error, China, Chengdu, 32G', // sending proposal to storage provider failed: failed to dial
-  t03203: 'error, China, Wuhan, 32G, 12h', // sending proposal to storage provider failed: failed to dial
+  t03653: 'error, Canada, Embrun, 32G', // deal failed: (State=26) handing off deal to node: AddPiece failed: adding piece to sector: writing piece: maybeSchedRequest didn't find any good workers
+  t03733: 'error, Korea, Uijeongbu-si, 32G', // deal failed: (State=26) deal data verification failed: error generating CommP: Rust panic: no unwind information
 
   // Stuck
-  t01235: 'transferring, Canada, Vancouver, 512M, @jimpick, 1h',
-  t01372: 'transferring, China, Shijiazhuang, 512M, 2h',
-  t01407: 'transferring, Korea, Geumcheon-gu, 512M, 2h, sat',
-  t02836: 'transferring, China, Shenzhen, 512M, 3h, sat',
-  t02944: 'transferring, China, Guilin, 512M, fri, sat',
-  t03440: 'transferring, China, Chengdu, 32G, sat',
+  t01372: 'error, China, Shijiazhuang, 512M, 2h', // xfr
+  t01407: 'error, Korea, Geumcheon-gu, 512M, 2h, sat', // xfr
+  t02836: 'error, China, Shenzhen, 512M, 3h, sat', // xfr
  
   // XNR
 
   // NR
+  t01032: 'NR, China, Hangzhou, 32G, 11h, fri',
   t01207: 'NR, Korea, Yangcheon-gu, 32G',
+  t01339: 'NR, China, Shanghai, 32G, 11h, fri',
   t01392: 'NR, Korea, Guro-gu, 512M, 2h',
   t01418: 'NR, China, Shandong, 512M, thu',
   t01476: 'NR, China, Xianning, 32G',
@@ -84,7 +94,12 @@ const annotations = {
   t02193: 'NR, China, Yibin, 512M, fri',
   t02507: 'NR, China, Zhongshan, 512M, fri',
   t02840: 'NR, China, Changsha, 512M, 2h',
+  t02940: 'NR, China, Zhongshan, 32G, fri',
   t03158: 'NR, China, Changsha, 32G',
+  t03266: 'NR, China, Shandong, 512M, 2h, fri, sat',
+  t03414: 'NR, Germany, Frankfurt, 32G, fri',
+  t03438: 'NR, China, Luzhou, 32G, fri',
+  t03550: 'NR, China, Changsha, 512M, sat',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
