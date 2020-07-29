95a7c2519aed7ff400952c08d7f6f8c25350729c
Jim Pick <jim@backblend.com>
2020-07-29 09:05:30 -0700

    Active deals!

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -1,41 +1,41 @@
 const annotations = {
   // Active
+  t01047: 'active, China, Zhejiang, 32G, 9h',
+  t02329: 'active, China, Hangzhou, 512M, 2h',
+  t03705: 'active, China, Shenzhen, 512M, 1h',
+  t06144: 'active, China, Beijing, 512M, 1h',
+  t06187: 'active, China, Zhongshan, 512M, 1h',
+  t07253: 'active, China, Zhongshan, 512M, 1h',
 
   // Sealing
-  t01047: 'sealing, China, Zhejiang, 32G, mon',
-  t02329: 'sealing, China, Hangzhou, 512M, mon',
-  t03705: 'sealing, China, Shenzhen, 512M, mon',
-  t05182: 'sealing, China, Wuxi, 32G, mon',
-  t05183: 'sealing, China, Wuxi, 32G, mon',
-  t05184: 'sealing, China, Wuxi, 32G, mon',
-  t05185: 'sealing, China, Wuxi, 32G, mon',
-  t06144: 'sealing, China, Beijing, 512M, mon',
-  t06187: 'sealing, China, Zhongshan, 512M, mon',
+  t05182: 'sealing, China, Wuxi, 32G, tue',
+  t05183: 'sealing, China, Wuxi, 32G, tue',
+  t05184: 'sealing, China, Wuxi, 32G, tue',
+  t05185: 'sealing, China, Wuxi, 32G, tue',
+  t05987: 'sealing, tue', // On-chain
 
   // New, sealing, recycle
   // t01060: 'new',
   // t01095: 'new',
   // t01513: 'new',
-  t01915: 'xnew, China, Zhongshan, 512M',
-  t02251: 'xnew, China, Shenzhen, 512M',
   // t02471: 'new',
   // t02681: 'new',
-  t02974: 'xnew, China, Shijiazhuang, 512M',
-  t03335: 'xnew, Australia, Moggill, 32G',
-  t03678: 'xnew, China, Shijiazhuang, 512M',
   // t04120: 'new',
   // t04263: 'new',
-  t04278: 'xnew, Canada, Vancouver, 512M, @jimpick',
-  t05987: 'xnew', // On-chain
-  t07253: 'xnew, China, Zhongshan, 512M',
 
   // Asks
 
   // No-online deals
 
   // Error
+  t01915: 'error, China, Zhongshan, 512M', // sending proposal to storage provider failed: failed to dial
+  t02251: 'error, China, Shenzhen, 512M',
+  t02974: 'error, China, Shijiazhuang, 512M', // deal failed: (State=26) error calling node: AddFunds errored: handler: websocket connection closed
+  t03335: 'error, Australia, Moggill, 32G', // failed to initiate data transfer: deal data transfer failed
 
   // Stuck
+  t03678: 'stuck, China, Shijiazhuang, 512M', // cfa
+  t04278: 'stuck, Canada, Vancouver, 512M, @jimpick', // cfa
  
   // XNR
 
