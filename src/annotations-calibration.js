cb29fd12928bf5c2cb10453a5fa61c00f1f5ccb9
Jim Pick <jim@backblend.com>
2020-07-29 18:55:45 -0700

    More miners, deals

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -6,7 +6,7 @@ const annotations = {
   t04278: 'active, Canada, Vancouver, 512M, @jimpick, 1h',
   t06144: 'active, China, Beijing, 512M, 1h',
   t06187: 'active, China, Zhongshan, 512M, 1h',
-  t06866: 'active, maddr only, 512M, 2h',
+  t06866: 'active, China, Hangzhou, 512M, 2h',
   t07253: 'active, China, Zhongshan, 512M, 1h',
   t014888: 'active, China, Zhongshan, 512M, 1h',
   t015485: 'active, China, Shijiazhuang, 512M, 1h',
@@ -42,31 +42,34 @@ const annotations = {
   t010487: 'sealing, China, Wuxi, 32G, wed',
   t010624: 'sealing, Sweden, Stockholm, 32G, wed',
   t010936: 'sealing, China, Shandong, 32G, wed',
+  t012169: 'sealing, China, Guilin, 32G, wed',
   t013370: 'sealing, China, Fujian, 512M, wed',
+  t014805: 'sealing, China, Luzhou, 32G, wed',
   t015036: 'sealing, China, Shijiazhuang, 512M, wed',
+  t015323: 'sealing, China, Shanghai, 32G, wed',
   t016827: 'sealing, China, Changsha, 512M, wed',
   t017625: 'sealing, China, Fujian, 512M, wed',
+  t018018: 'sealing, China, Yibin, 32G, wed',
   t018901: 'sealing, China, Shenzhen, 512M, wed',
+  t019908: 'sealing, Germany, Frankfurt, 32G, wed',
+  t019954: 'sealing, China, Jinan, 32G, wed',
+  t019960: 'sealing, USA, Rochester, 512M, wed',
+  t020047: 'sealing, China, Shanghai, 32G, wed',
+  t020089: 'sealing, China, Shanghai, 32G, wed',
+  t020117: 'sealing, Russia, 32G, wed',
+  t020204: 'sealing, USA, Kellogg, 32G, wed',
 
   // New, recycle
-  t012169: 'new, China, Guilin, 32G',
-  t014692: 'new, Netherlands, Amersfoort, 32G',
-  t014805: 'new, China, Luzhou, 32G',
-  t015323: 'new, China, Shanghai, 32G',
-  t015579: "new, China, Xi'an, 32G",
-  t017888: 'new, China, Wuxi, 32G',
-  t017935: 'new, Netherlands, Purmerend, 32G',
-  t017977: 'new, USA, Ashburn, 512M',
-  t018018: 'new, China, Yibin, 32G',
-  t019908: 'new, Germany, Frankfurt, 32G',
-  t019954: 'new, China, Jinan, 32G',
-  t019960: 'new, USA, Rochester, 512M',
-  t020047: 'new, China, Shanghai, 32G',
-  t020089: 'new, China, Shanghai, 32G',
-  t020106: 'new, USA, Grand Rapids, 512M',
-  t020117: 'new, Russia, 32G',
-  t020185: 'new, China, Shanghai, 32G',
-
+  t04448: 'stuck, China, Hangzhou, 32G', // cfa
+  t05323: "new, China, Xi'an, 32G",
+  t06072: 'new, China, Wuhan, 32G',
+  t06650: 'stuck, China, Hangzhou, 512M', // cfa
+  t06733: 'stuck, China, Beijing, 32G', // cfa
+  t06818: 'stuck, Korea, Bucheon-si, 512M', // cfa
+  t07576: 'new, China, Wuxi, 32G',
+  t09019: 'stuck, Korea, Geumcheon-gu, 512M', // cfa
+  t09065: 'stuck, Korea, Geumcheon-gu, 512M', // cfa
+ 
   // maddr only
   
   // Asks
@@ -74,35 +77,45 @@ const annotations = {
   // No-online deals
 
   // Error
-  t01434: 'error, China, Hong Kong, 512M', // nr
-  t01915: 'error, China, Zhongshan, 512M', // sending proposal to storage provider failed: failed to dial
   t02039: 'error, China, Hong Kong, 32G', // deal failed: (State=26) handing off deal to node: AddPiece failed: adding piece to sector: writing piece: no suitable P1 workers found
-  t02251: 'error, China, Shenzhen, 512M', // sending proposal to storage provider failed: failed to dial
-  t02974: 'error, China, Shijiazhuang, 512M', // deal failed: (State=26) error calling node: AddFunds errored: handler: websocket connection closed
-  t04280: 'error, maddr only, 512M', // nr
-  t04904: 'error, China, Shijiazhuang, 512M', // sending proposal to storage provider failed: failed to dial
-  t05017: 'error, China, Shijiazhuang, 32G', // sending proposal to storage provider failed: failed to dial
+  t04645: 'error, China, Beijing, 32G', // dial backoff
   t05235: 'error, maddr only, 32G', // nr
+  t06730: 'error, China, Yibin, 512M', // stream reset
   t07487: 'error, maddr only, 32G', // nr
+  t08883: 'error, China, Shenzhen, 512M', // failed to dial
   t09466: 'error, maddr only, 32G', // sending proposal to storage provider failed: failed to dial
   t010483: 'error, maddr only, 512M', // nr
-  t013628: 'error, maddr only, 512M', // nr
-  t015164: 'error, maddr only, 512M', // sending proposal to storage provider failed: dial backoff
+  t015579: "error, China, Xi'an, 32G", // dial backoff
 
   // Stuck
   t02712: 'stuck, China, Shenzhen, 32G', // cfa
   t03335: 'stuck, Australia, Moggill, 32G', // cfa
-  t03678: 'stuck, China, Shijiazhuang, 512M', // cfa
   t04143: 'stuck, China, Beijing, 32G', // cfa
   t04366: 'stuck, China, Chengdu, 32G', // cfa
-  t04645: 'stuck, China, Beijing, 32G', // cfa
+  t04904: 'stuck, China, Shijiazhuang, 512M', // cfa
+  t05017: 'stuck, China, Shijiazhuang, 32G', // cfs
   t06734: 'stuck, China, Guangzhou, 32G', // cfs
-  t07191: 'stuck, China, Guyuan, 32G', // cfa
   t07349: 'stuck, China, Guangzhou, 32G', // cfa
   t09331: "stuck, China, Xi'an, 512M", // xfr
   t09507: 'stuck, China, Changsha, 512M', // cfa
+  t014692: 'stuck, Netherlands, Amersfoort, 32G', // cfa
+  t017888: 'stuck, China, Wuxi, 32G', // cfa
+  t017935: 'stuck, Netherlands, Purmerend, 32G', // cfa
+  t020106: 'stuck, USA, Grand Rapids, 512M', // cfa
+  t020185: 'stuck, China, Shanghai, 32G', // cfa
+
  
   // XNR
+  t01434: 'xnr, China, Hong Kong, 512M',
+  t01915: 'xnr, China, Zhongshan, 512M',
+  t02251: 'xnr, China, Shenzhen, 512M',
+  t02974: 'xnr, China, Shijiazhuang, 512M',
+  t03678: 'xnr, China, Shijiazhuang, 512M',
+  t04280: 'xnr, maddr only, 512M',
+  t07191: 'xnr, China, Guyuan, 32G',
+  t013628: 'xnr, maddr only, 512M',
+  t015164: 'xnr, maddr only, 512M',
+  t017977: 'xnr, USA, Ashburn, 512M',
 
   // NR
 
