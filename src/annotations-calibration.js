24df44dcd2ac4faa0089491f2034bf35df5c1f48
Jim Pick <jim@backblend.com>
2020-08-07 22:07:27 -0700

    More miners

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -6,20 +6,28 @@ const annotations = {
 
   t01054: 'sealing, China, Hong Kong, 512M, fri',
   t01058: 'sealing, China, Zhongshan, 512M, fri',
+  t01124: 'sealing, China, Jiaxing, 32G, fri',
+  t01127: 'sealing, China, Guangdong, 32G, fri',
+  t01183: 'sealing, Canada, Vancouver, 512M, @jimpick, fri',
   t01184: "sealing, China, Xi'an, 32G, fri",
+  t01202: 'sealing, China, Hong Kong, 32G, fri',
+  t01206: 'sealing, China, Shandong, 32G, fri',
+  t01217: 'sealing, China, Guilin, 512M, fri',
 
   // New, recycle
 
   t01115: 'new, China, Shijiazhuang, 512M',
   t01119: 'new, Korea, Seongnam-si, 32G',
-  t01124: 'new, China, Jiaxing, 32G',
-  t01127: 'new, China, Guangdong, 32G',
-  t01183: 'new, Canada, Vancouver, 512M, @jimpick',
-  t01202: 'new, China, Hong Kong, 32G',
-  t01206: 'new, China, Shandong, 32G',
   t01208: 'new, China, Sichuan, 32G',
-  t01217: 'new, China, Guilin, 512M',
   t01233: "new, USA, Coeur d'Alene, 512M, @why",
+  t01270: 'new, China, Weifang, 512M',
+  t01278: 'new, Australia, 32G',
+  t01305: 'new, China, Guangzhou, 32G',
+  t01314: 'new, Korea, Uijeongbu-si, 512M',
+  t01319: 'new, Korea, Dongjak-gu, 32G',
+  t01324: 'new, Wuxi, 32G',
+  t01325: 'new, Wuxi, 32G',
+  t01340: 'new, China, Guangzhou, 32G',
 
   // Asks
 
