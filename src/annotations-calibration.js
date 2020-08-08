1e42363d3ff1d5ef18be47a7e8cc4703517ec3b5
Jim Pick <jim@backblend.com>
2020-08-07 21:36:28 -0700

    More miners

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -4,10 +4,22 @@ const annotations = {
 
   // Sealing - fri
 
+  t01054: 'sealing, China, Hong Kong, 512M, fri',
+  t01058: 'sealing, China, Zhongshan, 512M, fri',
+  t01184: "sealing, China, Xi'an, 32G, fri",
+
   // New, recycle
 
-  t01054: 'new, China, Hong Kong, 512M',
-  t01058: 'new, China, Zhongshan, 512M',
+  t01115: 'new, China, Shijiazhuang, 512M',
+  t01119: 'new, Korea, Seongnam-si, 32G',
+  t01124: 'new, China, Jiaxing, 32G',
+  t01127: 'new, China, Guangdong, 32G',
+  t01183: 'new, Canada, Vancouver, 512M, @jimpick',
+  t01202: 'new, China, Hong Kong, 32G',
+  t01206: 'new, China, Shandong, 32G',
+  t01208: 'new, China, Sichuan, 32G',
+  t01217: 'new, China, Guilin, 512M',
+  t01233: "new, USA, Coeur d'Alene, 512M, @why",
 
   // Asks
 
