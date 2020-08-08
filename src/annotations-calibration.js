bf459b1afc4a3b37c0c6d4900a7ab235dbbf4d1e
Jim Pick <jim@backblend.com>
2020-08-08 08:19:15 -0700

    Active deals

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -4,25 +4,29 @@ const annotations = {
 
   t01054: 'active, China, Hong Kong, 512M, 2h',
   t01058: 'active, China, Zhongshan, 512M, 1h',
+  t01115: 'active, China, Shijiazhuang, 512M, 2h',
+  t01138: 'active, China, Shijiazhuang, 512M, <8h',
   t01183: 'active, Canada, Vancouver, 512M, @jimpick, 2h',
+  t01184: "active, China, Xi'an, 32G, <11h",
   t01217: 'active, China, Guilin, 512M, 2h',
+  t01233: "active, USA, Coeur d'Alene, 512M, @why, 2h, 2h",
+  t01270: 'active, China, Weifang, 512M, 2h',
+  t01314: 'active, Korea, Uijeongbu-si, 512M, <10h',
+  t01364: 'active, China, Shijiazhuang, 512M, <9h',
+  t01378: 'active, China, Ordos, 512M, <9h',
+  t01444: 'active, China, Zhongshan, 512M, <9h',
+  t01500: 'active, China, Chengdu, 512M, <8h',
 
   // Sealing - fri
 
-  t01115: 'sealing, China, Shijiazhuang, 512M, fri',
   t01119: 'sealing, Korea, Seongnam-si, 32G, fri',
   t01124: 'sealing, China, Jiaxing, 32G, fri',
   t01127: 'sealing, China, Guangdong, 32G, fri',
-  t01138: 'sealing, China, Shijiazhuang, 512M, fri',
-  t01184: "sealing, China, Xi'an, 32G, fri",
   t01202: 'sealing, China, Hong Kong, 32G, fri',
   t01206: 'sealing, China, Shandong, 32G, fri',
   t01208: 'sealing, China, Sichuan, 32G, fri',
-  t01233: "sealing, USA, Coeur d'Alene, 512M, @why, fri",
-  t01270: 'sealing, China, Weifang, 512M, fri',
   t01305: 'sealing, China, Guangzhou, 32G, fri',
   t01306: 'sealing, Japan, Tokushima, 32G, fri',
-  t01314: 'sealing, Korea, Uijeongbu-si, 512M, fri',
   t01319: 'sealing, Korea, Dongjak-gu, 32G, fri',
   t01325: 'sealing, China, Wuxi, 32G, fri',
   t01329: 'sealing, Canada, Brampton, 32G, fri',
@@ -30,11 +34,9 @@ const annotations = {
   t01341: 'sealing, China, Wuxi, 32G, fri',
   t01352: 'sealing, Germany, Frankfurst + China, Beijing, 32G, fri',
   t01362: 'sealing, China, Beijing, 32G, fri',
-  t01364: 'sealing, China, Shijiazhuang, 512M, fri',
   t01365: 'sealing, Korea, Seoul, 32G, fri',
   t01366: 'sealing, China, Shijiazhuang, 32G, fri',
   t01371: 'sealing, China, Beijing, 32G, fri',
-  t01378: 'sealing, China, Ordos, 512M, fri',
   t01383: 'sealing, China, Wuxi, 32G, fri',
   t01384: 'sealing, China, Wuxi, 32G, fri',
   t01403: 'sealing, China, Shenzhen, 32G, fri',
@@ -42,22 +44,21 @@ const annotations = {
   t01409: 'sealing, China, Chengdu, 32G, fri',
   t01414: 'sealing, Denmark, Kobenhavn S, 32G, fri',
   t01419: 'sealing, China, Tongling, 32G, fri',
-  t01444: 'sealing, China, Zhongshan, 512M, fri',
   t01455: 'sealing, China, Guangdong, chain only, 32G, fri',
   t01484: 'sealing, China, Beijing, 512M, fri',
   t01485: 'sealing, China, Beijing, 32G, fri',
   t01487: 'sealing, China, Zhejiang, 512M, fri',
   t01496: 'sealing, Kenya, Nairobi, 32G, fri',
-  t01500: 'sealing, China, Chengdu, 512M, fri',
+  t01513: 'sealing, China, Shanghai, 32G, fri',
   t01547: 'sealing, China, Hangzhou, 32G, fri',
   t01548: 'sealing, China, Shenzhen, 32G, fri',
   t01549: 'sealing, China, Hong Kong, 32G, fri',
   t01553: 'sealing, China, Chongqing, 32G, fri',
+  t01559: 'sealing, China, Guangzhou, 32G, fri',
   t01571: 'sealing, China, Beijing, 32G, fri',
 
   // New, recycle
 
-  t01559: 'stuck, China, Guangzhou, 32G', // cfa
 
   // Asks
 
@@ -71,7 +72,6 @@ const annotations = {
   t01345: 'error, Australia, 32G', // out of gas
   t01464: 'error, Germany, Hamburg, 32G', // out of gas
   t01478: 'error, China, Guangzhou, 512M', // failed to dial
-  t01513: 'error, China, Shanghai, 32G', // failed to dial
   t01546: 'error, Netherlands, 32G', // out of gas
 
 
