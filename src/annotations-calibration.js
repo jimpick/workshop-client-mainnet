0f087b5c23e3be86f0a50dd66790f54da72afb21
Jim Pick <jim@backblend.com>
2020-08-20 10:54:22 -0700

    New deals

--- src/annotations-calibration.js
+++ src/annotations-calibration.js
@@ -2,35 +2,57 @@ const annotations = {
 
   // Active - wed
   
+  t01327: 'active, China, Guangdong, 512M, 2h',
+  t01591: 'active, China, Beijing, 512M, 1h',
+  t01593: 'active, USA, Athol, 512M, @why, 2h',
+  t01699: 'active, Poland, Krakow, 512M, @magik6k, 2h',
+  t01708: 'active, China, Shenzhen, 512M, 2h',
+  t01819: 'active, China, Shijiazhuang, 512M, 2h',
+  t01824: 'active, China, Guilin, 512M, 2h',
+  t01827: 'active, China, Shenyang, 512M, 2h',
+  t01902: 'active, China, Shanghai, 512M, 2h',
+  t01920: 'active, China, Hong Kong + Jieyang + Shenzhen, 512M, 2h',
+  t01945: 'active, China, Shanghai, 512M, 2h',
+  t02014: 'active, Canada, Vancouver, 512M, @jimpick, 2h',
+  t02098: 'active, China, Guyuan, 512M, 2h',
+  t02125: 'active, USA, Concord, 512M, 4h',
+
+  t01017: 'active32, China, Shandong, 32G, 7h',
+  t01337: 'active32, China, Shandong, 32G, 10h',
+
   // Sealing - wed
  
-  t01017: 'sealing, China, Shandong, 32G, wed',
-  t01307: 'sealing, China, Fujian, 32G, wed',
-  t01308: 'sealing, Sweden, Alvsjo, 32G, wed',
-  t01310: 'sealing, China, Ningbo, 32G, wed',
-  t01327: 'sealing, China, Guangdong, 512M, wed',
-  t01337: 'sealing, China, Shandong, 32G, wed',
   t01581: 'sealing, Australia, Sydney, 512M, wed',
-  t01591: 'sealing, China, Beijing, 512M, wed',
-  t01593: 'sealing, USA, Athol, 512M, @why, wed',
-  t01699: 'sealing, Poland, Krakow, 512M, @magik6k, wed',
-  t01708: 'sealing, China, Shenzhen, 512M, wed',
-  t01819: 'sealing, China, Shijiazhuang, 512M, wed',
-  t01824: 'sealing, China, Guilin, 512M, wed',
-  t01827: 'sealing, China, Shenyang, 512M, wed',
-  t01902: 'sealing, China, Shanghai, 512M, wed',
-  t01920: 'sealing, China, Hong Kong + Jieyang + Shenzhen, 512M, wed',
-  t01945: 'sealing, China, Shanghai, 512M, wed',
-  t02000: 'sealing, China, Guangdong, 32G, wed',
-  t02002: 'sealing, China, Changzhou, 32G, wed',
-  t02007: 'sealing, China, Shenzhen, 32G, wed',
-  t02014: 'sealing, Canada, Vancouver, 512M, @jimpick, wed',
+
+  t01307: 'sealing32, China, Fujian, 32G, wed',
+  t01308: 'sealing32, Sweden, Alvsjo, 32G, wed',
+  t01310: 'sealing32, China, Ningbo, 32G, wed',
+  t02000: 'sealing32, China, Guangdong, 32G, wed',
+  t02002: 'sealing32, China, Changzhou, 32G, wed',
+  t02007: 'sealing32, China, Shenzhen, 32G, wed',
 
   // Stuck - wed
  
 
   // New
 
+  t02182: 'new, China, Zhongshan, 512M',
+  t02197: 'new, China, Taizhou, 512M',
+  t02206: 'new, USA, Boardman, 512M',
+  t02231: 'new, UK, Lower Slaughter, 512M',
+  t02249: 'new, China, Guangdong, 512M',
+  t02263: 'new, USA, Boardman, 512M',
+  t02264: 'new, USA, Boardman, 512M',
+  t02315: 'new, USA, Boardman, 512M',
+  t02301: 'new, China, Liaoning, 512M',
+  t02302: 'new, China, Xiamen, 512M',
+  t02306: 'new, China, Shijiazhuang, 512M',
+  t02533: 'new, France, Aubervilliers, 512M',
+  t02553: 'new, China, Shenzhen, 512M',
+  t02555: 'new, China, Guiyang, 512M',
+  t02556: 'new, China, Xiamen, 512M',
+  t02641: 'new, China, Shenzhen, 512M',
+  t02673: 'new, China, Shandong, 512M',
 
 
   // Asks
