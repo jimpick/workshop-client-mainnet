7f13388593702c2383698068e627d48f2a77e4df
Jim Pick <jim@jimpick.com>
2020-08-24 21:41:22 -0700

    updates

--- src/annotations-spacerace.js
+++ src/annotations-spacerace.js
@@ -13,12 +13,11 @@ const annotations = {
   t01102: 'sealing, China, Shandong, mon',
   t01152: 'sealing, China, Beijing, mon',
   t01156: 'sealing, China, Hangzhou, mon',
-  t01232: 'sealing, China, Hangzhou, mon',
-  t01247: 'sealing, Canada, Montreal, mon',
-  t01272: 'sealing, Singapore, mon',
+  t01232: 'sealing, China, Hangzhou, mon', // xnr?
   t01274: 'sealing, China, Guangdong, mon',
   t01288: 'sealing, China, Yibin, mon',
   t01799: 'sealing, China, Jinan, mon',
+  t02304: 'sealing, USA, Mountain View, mon',
   t02381: 'sealing, China, Dongguan, mon',
   t02382: 'sealing, China, Zhongshan, mon',
   t02385: 'sealing, USA, East Islip, mon',
@@ -28,7 +27,6 @@ const annotations = {
   t02424: 'sealing, Korea, Wanju, mon',
   t02518: 'sealing, China, Shaanxi, mon',
   t02540: 'sealing, USA, Rochester, mon',
-  t02574: 'sealing, Japan, Minamata, mon',
   t02575: 'sealing, USA, Davis, mon',
   t02608: 'sealing, USA, San Jose, mon',
   t02614: 'sealing, Singapore, mon',
@@ -37,13 +35,11 @@ const annotations = {
   t02665: 'sealing, Netherlands, Amsterdam, mon',
   t03140: 'sealing, China, QingDao, mon',
   t03143: 'sealing, China, Shijiazhuang + Hong Kong, mon',
-  t03149: 'sealing, USA, Newton, mon',
   t03194: 'sealing, China, Shenzhen, mon',
-  t03225: 'sealing, China, Guyuan, mon',
   t03264: 'sealing, Korea, Bucheon-si, mon',
   t03314: 'sealing, China, Zhangjiakou, mon',
   t03339: 'sealing, China, Luzhou, mon',
-  t03340: "sealing, China, Xi, monan",
+  t03340: "sealing, China, Xi, mon",
   t03344: 'sealing, China, Guangzhou, mon',
   t03345: 'sealing, China, Fuzhou, mon',
   t03351: 'sealing, China, Chengdu, mon',
@@ -56,7 +52,6 @@ const annotations = {
   t07709: 'sealing, Korea, Dongjak-gu, mon',
   t07710: 'sealing, Hong Kong, mon', 
   t07717: 'sealing, China, Shenyang, mon',
-  t07796: 'sealing, China, Hunan, mon',
   t07806: 'sealing, China, Wuhan, mon',
   t07848: 'sealing, China, Wuhan, mon',
   t07857: 'sealing, China, Zhengzhou, mon',
@@ -65,6 +60,7 @@ const annotations = {
   t07956: 'sealing, Russia, mon',
   t07961: 'sealing, China, Changzhou, mon',
   t07964: 'sealing, USA, Ashburn, mon',
+  t08097: 'sealing, Korea, Pyeongtaek-si, mon',
 
 
   // Stuck - mon
@@ -77,10 +73,14 @@ const annotations = {
 
   // Error
 
+  t01272: 'error, Singapore, mon', // reset
+  t03225: 'error, China, Guyuan, mon', // protocol not supported
 
   // Dial backoff
 
+  t01247: 'backoff, Canada, Montreal, mon',
   t02506: 'backoff, USA, Clarksville',
+  t03149: 'backoff, USA, Newton, mon',
   t03305: 'backoff, Kenya, Nairobi',
   t03306: 'backoff, Australia',
   t03307: 'backoff, Australia',
@@ -103,7 +103,9 @@ const annotations = {
   t01279: 'rejected, China, Sichuan',
   t01280: 'rejected, China, Wuxi',
   t01287: 'rejected, China, Jiaxing',
+  t01782: 'rejected, China, Hong Kong + Singapore + USA, Portland',
   t02303: 'rejected, China, Hong Kong + Jiangsu + Netherlands',
+  t02398: 'rejected, China, Hong Kong',
   t02405: 'rejected, China, Shenzhen',
   t02416: 'rejected, China, Guangdong',
   t02417: 'rejected, China, Liaoning',
@@ -116,6 +118,7 @@ const annotations = {
   t02520: 'rejected, China, Hong Kong + Dongguan',
   t02528: 'rejected, China, Hong Kong + Inner Mongolia',
   t02534: 'rejected, China, Beijing',
+  t02574: 'rejected, Japan, Minamata, mon',
   t02606: 'rejected, China, Shanghai',
   t02607: 'rejected, China, Beijing',
   t02619: 'rejected, China, Beijing',
@@ -131,20 +134,27 @@ const annotations = {
   t07851: 'rejected, Netherlands, Purmerend',
   t07866: 'rejected, China, Foshan',
   t07919: 'rejected, China, Beijing, Shanghai, Hunan',
+  t08095: 'rejected, Canada, Surrey',
+  t08096: 'rejected, China, Wuhan',
+
 
   // Dial errors
 
   t01229: 'dial, China, Hong Kong',
   t02533: 'dial, Spain, Terrassa, mon',
+  t02577: 'dial, China, Hong Kong + Russia, Khabarovsk + Japan, Heiwajima',
   t03269: 'dial, Australia',
   t03302: 'dial, China, Chengdu',
   t03327: 'dial, Kenya, Nairobi',
   t03766: 'dial, China, Weifang, mon',
   t07819: 'dial, China, Wuxi',
+  t08032: 'dial, China, Weifang',
+  t08101: 'dial, China, Wuxi',
 
   // XNR
 
-  t02436: 'xnr, Netherlands', // AddPiece failed
+  t02436: 'xnr, Netherlands',
+  t07796: 'xnr, China, Hunan, mon',
 
   // Bootstrappers
   t01000: 'NR - bootstrap',
