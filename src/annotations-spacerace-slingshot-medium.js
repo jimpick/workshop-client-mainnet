c8d17a29b43e4fcdccf67c4749f6d6888da7be9e
Jim Pick <jim@jimpick.com>
2020-10-04 13:04:22 -0700

    Add candidates for slingshot-medium

---
+++ src/annotations-spacerace-slingshot-medium.js
@@ -0,0 +1,167 @@
+// Slingshot test results
+//
+// Additional testing with files > 128MiB in size
+
+const annotations = {
+  // Candidates
+
+  t01152: 'candidate, China, Beijing',
+  t01238: 'candidate, Vietnam, Hanoi, FILECOIN-VIETNAM', // 131072 < 134217728
+  t01241: 'candidate, USA, Portland, 6block',
+  t01272: 'candidate, Singapore',
+  t01278: 'candidate, USA, Grand Rapids, MiMiner', // 131072 < 134217728
+  t01800: 'candidate, China, Shenzhen + Chengdu',
+  t02305: 'candidate, China, Dongguan',
+  t02381: 'candidate, China, Dongguan, 西野七濑迷叔',
+  t02387: 'candidate, Canada, Brampton, NBFS Canada',
+  t02388: 'candidate, Netherlands, Amersfoort, Kroketje', // 131072 < 134217728
+  t02399: 'candidate, China, Zhejiang',
+  t02405: 'candidate, China, Shenzhen',
+  t02415: 'candidate, China, Hong Kong',
+  t02423: 'candidate, Ukraine, Kyiv, igowreck',
+  t02500: 'candidate, Korea, Seongnam-si', // 131072 < 134217728
+  t02501: 'candidate, USA, Portland, 6block',
+  t02514: 'candidate, USA, Portland, 6block',
+  t02576: 'candidate, Denmark, Gjerlev, BenjaminH', // 131072 < 134217728
+  t02620: 'candidate, Poland, Krakow, @magik6k', // 20000000 < 114746093
+  t02622: 'candidate, USA, Portland, 6block',
+  t02623: 'candidate, USA, Portland, 6block',
+  t02633: 'candidate, China, Hong Kong',
+  t02645: 'candidate, China, Hong Kong',
+  t02654: 'candidate, China, Shanghai',
+  t02772: 'candidate, China, Chengdu',
+  t03131: 'candidate, China, Changsha',
+  t03143: 'candidate, China, Shijiazhuang + Hong Kong',
+  t03224: 'candidate, Ukraine, Irpin, oboltusov', // 131072 < 134217728
+  t03264: 'candidate, Korea, Bucheon-si', // 131072 < 134217728
+  t03273: 'candidate, USA, Portland, 6block',
+  t03274: 'candidate, USA, Portland, 6block',
+  t03275: 'candidate, USA, Portland, 6block',
+  t03302: 'candidate, China, Chengdu',
+  t03325: 'candidate, Germany, Frankfurt',
+  t03328: 'candidate, China, Xinxiang',
+  t03339: 'candidate, China, Luzhou, benxun',
+  t03340: "sealing, China, Xi'an",
+  t03347: 'candidate, China, Shenzhen',
+  t03363: 'candidate, Singapore',
+  t03482: 'candidate, China, Wuxi',
+  t03491: 'candidate, China, Hong Kong',
+  t03512: 'candidate, New Zealand, Auckland, NZFILECOIN01',
+  t03624: 'candidate, Germany, Chemnitz, ode',
+  t04443: 'candidate, China, Shanghai, 罗良科技',
+  t05315: 'candidate, USA, Portland, 6block',
+  t05316: 'candidate, USA, Portland, 6block',
+  t05317: 'candidate, USA, Portland',
+  t07709: 'candidate, Korea, Dongjak-gu', // 131072 < 134217728
+  t07850: 'candidate, USA, Portland, 6block',
+  t07919: 'candidate, China, Beijing, Shanghai, Hunan',
+  t07990: 'candidate, China, Hong Kong',
+  t08025: 'candidate, China, Hong Kong',
+  t08220: 'candidate, China, Shenzhen',
+  t08240: 'candidate, Russia, Novosibirsk, Rabinovitch', // 131072 < 134217728
+  t08264: 'candidate, China, Xianning', // 131072 < 134217728
+  t08311: 'candidate, China, Shenzhen',
+  t08371: 'candidate, China, Chengdu',
+  t08383: 'candidate, USA, Portland, 6block',
+  t08403: 'candidate, UK, Lower Slaughter', // 131072 < 134217728
+  t08482: 'candidate, USA, Athol, @why',
+  t09675: 'candidate, China, Weifang',
+  t09693: 'candidate, USA, Portland + Singapore',
+  t09696: 'candidate, USA, Portland + Singapore',
+  t09731: 'candidate, China, Guiyang',
+  t09833: 'candidate, USA, San Jose',
+  t09848: 'candidate, USA', // 131072 < 134217728
+  t010048: 'candidate, China, Shanghai',
+  t010084: 'candidate, China, Beijing',
+  t010241: 'candidate, China, Beijing + USA, Portland',
+  t010247: 'candidate, China, Yantai',
+  t010253: 'candidate, China, Beijing + USA, Portland',
+  t010438: 'candidate, USA', // 131072 < 134217728
+  t010446: 'candidate, Netherlands',
+  t010479: 'candidate, France, Fontenay-sous-Bois',
+  t010491: 'candidate, China, Shenzhen',
+  t010495: 'candidate, China, Tainjin',
+  t010501: 'candidate, China, Hangzhou',
+  t010507: 'candidate, China, Hangzhou',
+  t010512: 'candidate, China, Zhengzhou',
+  t010558: 'candidate, China, Shenzhen + Hong Kong',
+  t010617: 'candidate, Canada, Surrey, @feiya200', // 131072 < 134217728
+  t014394: 'candidate, China, Suzhou',
+  t014768: 'candidate, Singapore',
+  t015877: 'candidate, China, Jiaxing + USA',
+  t015897: 'candidate, China, Guangdong',
+  t015927: 'candidate, USA, East Islip', // 131072 < 134217728
+  t015941: 'candidate, Ukraine, Kyiv', // 131072 < 134217728
+  t016482: 'candidate, USA, Aliquippa',
+  t016594: 'candidate, Germany', // 131072 < 134217728
+  t017734: 'candidate, Australia, Sydney',
+  t018501: 'candidate, China, Beijing', // 131072 < 134217728
+  t018772: 'candidate, Australia',
+  t018780: 'candidate, Singapore + Germany, Frankfurt + USA',
+  t018781: 'candidate, Singapore + Germany, Frankfurt + USA',
+  t018782: 'candidate, Singapore + Germany, Frankfurt + USA',
+  t018783: 'candidate, Singapore + Germany, Frankfurt + USA',
+  t018784: 'candidate, Singapore + Germany, Frankfurt + USA',
+  t018785: 'candidate, Singapore + Germany, Frankfurt + USA',
+  t019022: "sealing, China, Xi'an",
+  t019029: 'candidate, Serbia, Boljevac', // 131072 < 134217728
+  t019041: 'candidate, Singapore',
+  t019074: 'candidate, China, Guangdong',
+  t019090: 'candidate, South Africa, Johannesburg',
+  t019100: 'candidate, Romania, Cluj-Napoca', // 131072 < 134217728
+  t019118: 'candidate, China, Jiaxing',
+  t019174: 'candidate, UK, Halifax',
+  t019240: 'candidate, China, Beijing',
+  t019243: 'candidate, USA, Jackson',
+  t019354: 'candidate, Germany, Frankfurt',
+  t019399: 'candidate, Korea, Busan',
+  t019459: 'candidate, USA, Alpharetta',
+  t020489: 'candidate, China, Lioaning',
+  t020523: 'candidate, China, Suzhou',
+  t020742: 'candidate, China, Shanghai + Hangzhou + Shaoxing',
+  t020747: 'candidate, China, Xiamen', // 131072 < 134217728
+  t020904: 'candidate, Korea, Uijeongbu-si', // 131072 < 134217728
+  t020923: 'candidate, China, Cangzhou',
+  t020975: 'candidate, China, Shandong',
+  t021328: 'candidate, China, Dongguan',
+  t021339: 'candidate, China, Jiaxing',
+  t021479: 'candidate, China, Qingdao',
+  t021540: 'candidate, Germany, Frankfurt, @hsanjuan',
+  t021583: 'candidate, China, Jiaxing',
+  t021710: 'candidate, China, Shijiazhuang',
+  t022070: 'candidate, China, Jieyang',
+  t022072: 'candidate, China, Jieyang',
+  t022289: 'candidate, China, Mianyang',
+  t022352: 'candidate, Norway, Borgen', // 131072 < 134217728
+  t022399: 'candidate, China, Beijing', // 131072 < 134217728
+  t022512: 'candidate, Canada, Embrun',
+  t022566: 'candidate, China, Nanchong',
+  t022755: 'candidate, China, Shenzhen',
+  t022820: 'candidate, China, Guangdong',
+  t022832: 'candidate, China',
+  t022836: 'candidate, China, Chongqing',
+  t022841: 'candidate, China, Zhongshan',
+  t022853: 'candidate, China, Fuzhou',
+  t022855: 'candidate, Finland, Helsinki', // 131072 < 134217728
+  t022922: 'candidate, China, Shenzhen',
+  t022969: 'candidate, China, Chengdu',
+  t023001: 'candidate, China, Changsha',
+  t023013: 'candidate, China, Hong Kong', // 20000000 < 24414062
+  t023207: 'candidate, undefined',
+  t023229: 'candidate, China, Shandong', // 131072 < 134217728
+  t023495: 'candidate, China, Beijing + USA, Portland',
+  t023526: 'candidate, China, Chongqing',
+  t023661: 'candidate, China, Zhejiang',
+  t023678: 'candidate, China, Jiaxing',
+  t023825: 'candidate, USA, Portland',
+  t023843: 'candidate, NR',
+  t023971: 'candidate, USA, @Brian Y',
+  t023986: 'candidate, Korea, Seoul',
+  t024015: 'candidate, USA, Portland',
+  t024016: 'candidate, USA, Portland',
+  t024070: 'candidate, Japan, Setagaya-ku', // 131072 < 134217728
+  t024074: 'candidate, NR',
+  t024129: 'candidate, China, Zhejiang'
+}
+
+export default annotations
