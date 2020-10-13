0986ffce80a2f210215621f0774a8ee88e5a2f7b
Jim Pick <jim@jimpick.com>
2020-10-12 17:41:50 -0700

    New slingshot results

--- src/annotations-spacerace-slingshot-medium.js
+++ src/annotations-spacerace-slingshot-medium.js
@@ -1,88 +1,21 @@
 // Slingshot test results
 //
 // Additional testing with files > 128MiB in size
+//
+// These are generated from a second round of testing after attempting
+// to store a small file, see annotations-spacerace.js for those results
 
 const annotations = {
   // Retest
 
   // Candidates
 
-  f01234: "min-ask, Belgium, De Klinge, Eliovp",
-  f01241: "testing, USA, Portland, 6block",
-  f01279: "min-ask, China, Sichuan",
-  f01280: "testing, China, Wuxi, 了凡超算",
-  f01287: "timeout-ask, China, Jiaxing, MaiTian",
-  f02381: "min-ask, China, Dongguan, 西野七濑迷叔",
-  f02416: "testing, China, Guangdong, 星际无限",
-  f02492: "testing, China, Shenzhen",
-  f02606: "testing, China, Shanghai",
-  f02654: "min-ask, China, Shanghai",
-  f02665: "testing, Netherlands, Amsterdam, fm-ops",
-  f02668: "timeout-ask, China, Chengdu",
-  f03222: "testing, China, Yuncheong, 星云矿池",
-  f03224: "testing, Ukraine, Irpin, oboltusov",
-  f03275: "testing, USA, Portland, 6block",
-  f03302: "testing, China, Chengdu",
-  f03345: "testing, China, Fuzhou, chh",
-  f03482: "testing, China, Wuxi",
-  f03512: "testing, New Zealand, Auckland, NZFILECOIN01",
-  f03544: "testing, China, Guiyang",
-  f07709: "min-ask, Korea, Dongjak-gu",
-  f07806: "testing, China, Wuhan, HelloXP",
-  f07824: "testing, China, Shanghai",
-  f07919: "min-ask, China, Beijing, Shanghai, Hunan",
-  f08094: "testing, China, Guangzhou, dongshao",
-  f08220: "testing, China, Shenzhen",
-  f08371: "testing, China, Chengdu",
-  f09569: "min-ask, China, Beijing",
-  f09770: "testing, China, Xi'an",
-  f010084: "testing, China, Beijing",
-  f010399: "testing, China, Guangzhou",
-  f010491: "testing, China, Shenzhen",
-  f010498: "testing, China, Shenzhen",
-  f010505: "timeout-ask, China, Wuhan",
-  f010513: "timeout-ask, China, Chongqing",
-  f010528: "timeout-ask, China, Chongqing",
-  f014394: "testing, China, Suzhou",
-  f015233: "testing, China, Nanjing",
-  f015941: "timeout-ask, Ukraine, Kyiv",
-  f017665: "testing, China, Shenyang",
-  f018783: "testing, Singapore + Germany, Frankfurt + USA",
-  f018784: "timeout-ask, Singapore + Germany, Frankfurt + USA",
-  f019041: "testing, Singapore",
-  f019074: "timeout-ask, China, Guangdong",
-  f019104: "testing, Canada, Chambly",
-  f019118: "testing, China, Jiaxing",
-  f019240: "testing, China, Beijing",
-  f019243: "testing, USA, Jackson",
-  f019661: "min-ask, Russia, Pyatigorsk",
-  f020489: "testing, China, Lioaning",
-  f020541: "testing, China, Shanghai",
-  f020608: "min-ask, Russia",
-  f022163: "testing, NR",
-  f022227: "testing, China, Jiangsu",
-  f022261: "testing, China, Luzhou",
-  f022376: "testing, China, Dongguan",
-  f022753: "testing, China, Hong Kong",
-  f022841: "testing, China, Zhongshan",
-  f022853: "testing, China, Fuzhou",
-  f022922: "testing, China, Shenzhen",
-  f023179: "testing, China, Fuzhou",
-  f023581: "min-ask, China, Hong Kong",
-  f023939: "testing, NR",
-  f024066: "testing, NR",
-  f024101: "testing, China, Guiyang",
-  f024127: "min-ask, China, Fuzhou",
-  f024468: "testing, China, Shenzhen",
-  f024550: "testing, China, Wenzhou",
-  f024800: "testing, China, Hong Kong",
-  f024969: "testing, NR",
-  f025007: "testing, Korea, Gyeonggi-do",
-  f029421: "testing, China, Hong Kong",
-  f029566: "testing, NR",
-  f030184: "min-ask, USA, Aliquippa",
-
   // Testing
+
+  f019240: 'testing, China, Beijing',
+  f022227: 'testing, China, Jiangsu',
+  f023939: 'testing, NR',
+  f024101: 'testing, China, Guiyang',
 
   // Active
 
@@ -102,15 +35,20 @@ const annotations = {
   f02301: 'sealing, USA',
   f02388: 'sealing, Netherlands, Amersfoort, Kroketje',
   f02401: 'sealing, Canada, Chambly, NBFS Canada',
+  f02416: 'sealing, China, Guangdong, 星际无限',
+  f02492: 'sealing, China, Shenzhen',
   f02501: 'sealing, USA, Portland, 6block',
   f02514: 'sealing, USA, Portland, 6block',
   f02620: 'sealing, Poland, Krakow, @magik6k',
   f02622: 'sealing, USA, Portland, 6block',
   f02623: 'sealing, USA, Portland, 6block',
+  f02665: 'sealing, Netherlands, Amsterdam, fm-ops',
   f03176: 'sealing, USA, San Mateo, Five Star-Helmsman&Heiben',
   f03223: 'sealing, USA',
   f03273: 'sealing, USA, Portland, 6block',
   f03274: 'sealing, USA, Portland, 6block',
+  f03275: 'sealing, USA, Portland, 6block',
+  f03302: 'sealing, China, Chengdu',
   f03340: "sealing, China, Xi'an",
   f03363: 'sealing, Singapore',
   f03488: 'sealing, Korea, Geumcheon-gu',
@@ -120,6 +58,7 @@ const annotations = {
   f07819: 'sealing, China, Wuxi, YC--wuxi',
   f07850: 'sealing, USA, Portland, 6block',
   f08019: 'sealing, China, Yantai, 三合',
+  f08094: 'sealing, China, Guangzhou, dongshao',
   f08157: 'sealing, China, Beijing + Germany, Frankfurt, 超星际',
   f08240: 'sealing, Russia, Novosibirsk, Rabinovitch',
   f08285: 'sealing, Latvia, Riga, stander',
@@ -132,6 +71,8 @@ const annotations = {
   f010241: 'sealing, China, Beijing + USA, Portland',
   f010253: 'sealing, China, Beijing + USA, Portland',
   f010446: 'sealing, Netherlands',
+  f010491: 'sealing, China, Shenzhen',
+  f010498: 'sealing, China, Shenzhen',
   f010507: 'sealing, China, Hangzhou',
   f010512: 'sealing, China, Zhengzhou',
   f014324: 'sealing, Canada, Vancouver',
@@ -140,10 +81,13 @@ const annotations = {
   f015877: 'sealing, China, Jiaxing + USA',
   f016594: 'sealing, Germany',
   f019100: 'sealing, Romania, Cluj-Napoca',
+  f019104: 'sealing, Canada, Chambly',
+  f019243: 'sealing, USA, Jackson',
   f019354: 'sealing, Germany, Frankfurt',
   f019362: 'sealing, China, Hangzhou',
   f019551: 'sealing, UK',
   f020398: 'sealing, China, Zhangjiakou',
+  f020541: 'sealing, China, Shanghai',
   f021255: 'sealing, China, Suzhou + Netherlands',
   f021274: 'sealing, China, Shanghai',
   f021339: 'sealing, China, Jiaxing',
@@ -157,6 +101,8 @@ const annotations = {
   f022125: 'sealing, China, Shenzhen',
   f022142: 'sealing, USA, Queens',
   f022289: 'sealing, China, Mianyang',
+  f022841: 'sealing, China, Zhongshan',
+  f022922: 'sealing, China, Shenzhen',
   f023152: 'sealing, China, Quanzhou',
   f023467: 'sealing, Norway, Oslo',
   f023495: 'sealing, China, Beijing + USA, Portland',
@@ -185,16 +131,20 @@ const annotations = {
   f024015: 'sealing, USA, Portland',
   f024016: 'sealing, USA, Portland',
   f024030: 'sealing, Korea, Gwangju',
+  f024066: 'sealing, NR',
   f024081: 'sealing, China, Fuzhou',
   f024084: 'sealing, China, Fuzhou',
   f024085: 'sealing, China, Fuzhou',
   f024130: 'sealing, Korea',
   f024136: 'sealing, China, Fuzhou',
   f024156: 'sealing, USA',
+  f024800: 'sealing, China, Hong Kong',
   f024944: 'sealing, Korea, Yeongdeungpo-dong',
+  f024969: 'sealing, NR',
   f025017: 'sealing, China, Hefei',
   f025019: 'sealing, China, Taiwan, Hsinchu',
   f029556: 'sealing, China, Wuxi',
+  f029566: 'sealing, NR',
   f029587: 'sealing, Hungary',
   f029595: 'sealing, China, Guangdong',
   f029628: 'sealing, Australia, Brisbane',
@@ -210,25 +160,56 @@ const annotations = {
 
   // Stuck
 
-  f03339: 'stuck, China, Luzhou, benxun', // Transferring: about 2 hours
-  f08073: 'stuck, China, Qiqihar, QY7TT', // Transferring: about 2 hours
-  f08204: 'stuck, USA, St. Louis', // ProposalAccepted: about 1 hour
-  f09639: 'stuck, China, Fuzhou', // CheckForAcceptance: about 4 hours
-  f010035: 'stuck, Netherlands', // Transferring: about 4 hours
-  f010247: 'stuck, China, Yantai', // CheckForAcceptance: about 4 hours
-  f010558: 'stuck, China, Shenzhen + Hong Kong', // Transferring: about 2 hours
-  f014281: 'stuck, Netherlands, Permerend', // ProposalAccepted: about 2 hours
-  f019824: 'stuck, USA', // CheckForAcceptance: about 7 hours
-  f020436: 'stuck, China, Beijing', // CheckForAcceptance: about 4 hours
-  f021532: 'stuck, China, Shanghai', // Transferring: about 4 hours
-  f022072: 'stuck, China, Jieyang', // Transferring: about 2 hours
-  f022287: 'stuck, China, Dongguan', // CheckForAcceptance: about 4 hours
+  f01241: 'stuck, USA, Portland, 6block', // Transferring: 14 minutes
+  f01280: 'stuck, China, Wuxi, 了凡超算', // Transferring: 34 minutes
+  f02606: 'stuck, China, Shanghai', // Transferring: 36 minutes
+  f03222: 'stuck, China, Yuncheong, 星云矿池', // Transferring: 28 minutes
+  f03224: 'stuck, Ukraine, Irpin, oboltusov', // Transferring: 10 minutes
+  f03339: 'stuck, China, Luzhou, benxun', // Transferring: about 3 hours
+  f03345: 'stuck, China, Fuzhou, chh', // Transferring: 34 minutes
+  f03482: 'stuck, China, Wuxi', // CheckForAcceptance: 36 minutes
+  f03512: 'stuck, New Zealand, Auckland, NZFILECOIN01', // CheckForAcceptance: 20 minutes
+  f03544: 'stuck, China, Guiyang', // Transferring: 34 minutes
+  f07806: 'stuck, China, Wuhan, HelloXP', // ProposalAccepted: 10 minutes
+  f07824: 'stuck, China, Shanghai', // Transferring: 30 minutes
+  f08073: 'stuck, China, Qiqihar, QY7TT', // Transferring: about 3 hours
+  f08204: 'stuck, USA, St. Louis', // ProposalAccepted: about 2 hours
+  f08220: 'stuck, China, Shenzhen', // Transferring: 10 minutes
+  f08371: 'stuck, China, Chengdu', // Transferring: 28 minutes
+  f09639: 'stuck, China, Fuzhou', // CheckForAcceptance: about 5 hours
+  f09770: "stuck, China, Xi'an", // CheckForAcceptance: 16 minutes
+  f010035: 'stuck, Netherlands', // Transferring: about 5 hours
+  f010084: 'stuck, China, Beijing', // Transferring: 34 minutes
+  f010247: 'stuck, China, Yantai', // CheckForAcceptance: about 5 hours
+  f010399: 'stuck, China, Guangzhou', // Transferring: 26 minutes
+  f010558: 'stuck, China, Shenzhen + Hong Kong', // Transferring: about 3 hours
+  f014281: 'stuck, Netherlands, Permerend', // ProposalAccepted: about 3 hours
+  f014394: 'stuck, China, Suzhou', // Transferring: 36 minutes
+  f015233: 'stuck, China, Nanjing', // Transferring: 34 minutes
+  f017665: 'stuck, China, Shenyang', // Transferring: 34 minutes
+  f018783: 'stuck, Singapore + Germany, Frankfurt + USA', // Transferring: 30 minutes
+  f019041: 'stuck, Singapore', // Transferring: 30 minutes
+  f019118: 'stuck, China, Jiaxing', // Transferring: 26 minutes
+  f019824: 'stuck, USA', // CheckForAcceptance: about 8 hours
+  f020436: 'stuck, China, Beijing', // CheckForAcceptance: about 5 hours
+  f020489: 'stuck, China, Lioaning', // Transferring: 14 minutes
+  f021532: 'stuck, China, Shanghai', // Transferring: about 5 hours
+  f022072: 'stuck, China, Jieyang', // Transferring: about 3 hours
+  f022261: 'stuck, China, Luzhou', // Transferring: 34 minutes
+  f022287: 'stuck, China, Dongguan', // CheckForAcceptance: about 5 hours
+  f022376: 'stuck, China, Dongguan', // ProposalAccepted: 10 minutes
+  f022753: 'stuck, China, Hong Kong', // CheckForAcceptance: 18 minutes
+  f022853: 'stuck, China, Fuzhou', // Transferring: 26 minutes
+  f023179: 'stuck, China, Fuzhou', // Transferring: 34 minutes
   f023501: 'stuck, China, Mianyang',
-  f023649: 'stuck, Korea, Seoul', // ProposalAccepted: 33 minutes
-  f029529: 'stuck, China, Dongguan', // ProposalAccepted: about 3 hours
-  f030327: 'stuck, China, Beijing', // ProposalAccepted: 39 minutes
-  f030347: 'stuck, China, Zhejiang', // Transferring: about 2 hours
-  f030391: 'stuck, China, Chengdu', // CheckForAcceptance: about 3 hours
+  f023649: 'stuck, Korea, Seoul', // ProposalAccepted: about 2 hours
+  f024468: 'stuck, China, Shenzhen', // Transferring: 20 minutes
+  f024550: 'stuck, China, Wenzhou', // CheckForAcceptance: 34 minutes
+  f025007: 'stuck, Korea, Gyeonggi-do', // CheckForAcceptance: 34 minutes
+  f029529: 'stuck, China, Dongguan', // ProposalAccepted: about 4 hours
+  f030327: 'stuck, China, Beijing', // ProposalAccepted: about 2 hours
+  f030347: 'stuck, China, Zhejiang', // Transferring: about 3 hours
+  f030391: 'stuck, China, Chengdu', // CheckForAcceptance: about 4 hours
 
   // Min-size
 
@@ -236,6 +217,19 @@ const annotations = {
   f014409: 'min-size, USA, Waxhaw', // 134217728 < 262144000
 
   // Min-ask
+
+  f01234: 'min-ask, Belgium, De Klinge, Eliovp',
+  f01279: 'min-ask, China, Sichuan',
+  f02381: 'min-ask, China, Dongguan, 西野七濑迷叔',
+  f02654: 'min-ask, China, Shanghai',
+  f07709: 'min-ask, Korea, Dongjak-gu',
+  f07919: 'min-ask, China, Beijing, Shanghai, Hunan',
+  f09569: 'min-ask, China, Beijing',
+  f019661: 'min-ask, Russia, Pyatigorsk',
+  f020608: 'min-ask, Russia',
+  f023581: 'min-ask, China, Hong Kong',
+  f024127: 'min-ask, China, Fuzhou',
+  f030184: 'min-ask, USA, Aliquippa',
 
   // Error
 
@@ -276,6 +270,7 @@ const annotations = {
   f020928: 'error, China, Deyang + Singapore', // adding market funds failed: AddFunds exit code: SysErrInsufficientFunds(6)
   f021265: 'error, China, Wuhan', // failed to initiate data transfer: failed to open push data channel: Unable to send request: failed to find any peer in table
   f021444: 'error, China, Guangzhou', // failed to initiate data transfer: deal data transfer failed: response rejected
+  f022163: 'error, NR', // deal failed: (State=26) error calling node: ensuring funds: mpool push: failed to push message: not enough funds (required: 0.021034073749718952 FIL, balance: 0.004670603878781322 FIL): not enough funds to execute transaction
   f022352: 'error, Norway, Borgen', // adding market funds failed: AddFunds exit code: SysErrInsufficientFunds(6)
   f022566: 'error, China, Nanchong', // failed to initiate data transfer: deal data transfer failed: response rejected
   f023200: 'error, China, Shanghai', // failed to initiate data transfer: deal data transfer failed: response rejected
@@ -289,16 +284,10 @@ const annotations = {
   f024147: 'error, China, Fuzhou', // failed to initiate data transfer: deal data transfer failed: response rejected
   f024148: 'error, China, Fuzhou', // failed to initiate data transfer: deal data transfer failed: response rejected
   f025025: 'error, China, Qingdao', // adding market funds failed: AddFunds exit code: SysErrInsufficientFunds(6)
+  f029421: 'error, China, Hong Kong', // sending proposal to storage provider failed: exhausted 5 attempts but failed to open stream, err: protocol not supported
   f030158: 'error, China, Hangzhou', // failed to initiate data transfer: deal data transfer failed: response rejected
   f030226: 'error, China, Fuzhou', // failed to initiate data transfer: deal data transfer failed: response rejected
   f030230: 'error, China, Sichuan', // failed to initiate data transfer: failed to open push data channel: Unable to send request: routing: not found
-
-  // Timeout during ask
-
-  f01799: 'timeout-ask, China, Jinan, TokenHome',
-  f03194: 'timeout-ask, China, Shenzhen, 成都云存&Npool',
-  f05664: 'timeout-ask, China, Shenzhen, 艾比特网络科技',
-  f09848: 'timeout-ask, USA',
 
   // Rejected
 
@@ -319,6 +308,21 @@ const annotations = {
   f010493: 'dial, China, Shanghai', // * [/ip4/218.78.187.146/tcp/21735] dial tcp4 218.78.187.146:21735: connect: connection refused
 
   // XNR
+
+  // Timeout during ask
+
+  f01287: 'timeout-ask, China, Jiaxing, MaiTian',
+  f01799: 'timeout-ask, China, Jinan, TokenHome',
+  f02668: 'timeout-ask, China, Chengdu',
+  f03194: 'timeout-ask, China, Shenzhen, 成都云存&Npool',
+  f05664: 'timeout-ask, China, Shenzhen, 艾比特网络科技',
+  f09848: 'timeout-ask, USA',
+  f010505: 'timeout-ask, China, Wuhan',
+  f010513: 'timeout-ask, China, Chongqing',
+  f010528: 'timeout-ask, China, Chongqing',
+  f015941: 'timeout-ask, Ukraine, Kyiv',
+  f018784: 'timeout-ask, Singapore + Germany, Frankfurt + USA',
+  f019074: 'timeout-ask, China, Guangdong'
 
   // Error during ask
 }
