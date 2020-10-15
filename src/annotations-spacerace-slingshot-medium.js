e283d42268ca0d6b2384f7480e57863293f34583
Jim Pick <jim@jimpick.com>
2020-10-14 23:17:39 -0700

    Revert

--- src/annotations-spacerace-slingshot-medium.js
+++ src/annotations-spacerace-slingshot-medium.js
@@ -11,20 +11,14 @@ const annotations = {
   // Candidates
 
   f01234: 'candidate, Belgium, De Klinge, Eliovp',
-  f01238: 'candidate, Vietnam, Hanoi, FILECOIN-VIETNAM',
   f01240: 'candidate, Netherlands, HidNand',
-  f01247: 'candidate, Canada, Montreal, BigChungus™',
-  f01272: 'candidate, Singapore',
-  f01278: 'candidate, USA, Grand Rapids, MiMiner',
+  f01241: 'candidate, USA, Portland, 6block',
   f01279: 'candidate, China, Sichuan',
   f01280: 'candidate, China, Wuxi, 了凡超算',
   f01287: 'candidate, China, Jiaxing, MaiTian',
   f01289: 'candidate, China, Hangzhou + USA',
-  f01782:
-    'candidate, China, Hong Kong + Singapore + USA, Portland, hellofil.com',
   f01799: 'candidate, China, Jinan, TokenHome',
   f02299: 'candidate, China, Dongguan, Yolo',
-  f02301: 'candidate, USA',
   f02381: 'candidate, China, Dongguan, 西野七濑迷叔',
   f02399: 'candidate, China, Zhejiang',
   f02405: 'candidate, China, Shenzhen',
@@ -32,125 +26,90 @@ const annotations = {
   f02419: 'candidate, China, Shanghai, yuantai',
   f02423: 'candidate, Ukraine, Kyiv, igowreck',
   f02490: 'candidate, China, Fujian',
-  f02620: 'candidate, Poland, Krakow, @magik6k',
-  f02622: 'candidate, USA, Portland, 6block',
-  f02623: 'candidate, USA, Portland, 6block',
+  f02501: 'candidate, USA, Portland, 6block',
+  f02514: 'candidate, USA, Portland, 6block',
+  f02576: 'candidate, Denmark, Gjerlev, BenjaminH',
   f02625: 'candidate, China, Shanghai, IPFS星际工厂',
-  f02645: 'candidate, China, Hong Kong',
+  f02633: 'candidate, China, Hong Kong',
   f02822: 'candidate, China, Chengdu',
   f02824: 'candidate, China, Chengdu',
-  f03176: 'candidate, USA, San Mateo, Five Star-Helmsman&Heiben',
   f03194: 'candidate, China, Shenzhen, 成都云存&Npool',
   f03222: 'candidate, China, Yuncheong, 星云矿池',
   f03224: 'candidate, Ukraine, Irpin, oboltusov',
-  f03273: 'candidate, USA, Portland, 6block',
-  f03274: 'candidate, USA, Portland, 6block',
-  f03275: 'candidate, USA, Portland, 6block',
-  f03302: 'candidate, China, Chengdu',
   f03339: 'candidate, China, Luzhou, benxun',
   f03345: 'candidate, China, Fuzhou, chh',
-  f03363: 'candidate, Singapore',
-  f03488: 'candidate, Korea, Geumcheon-gu',
   f03491: 'candidate, China, Hong Kong',
-  f05316: 'candidate, USA, Portland, 6block',
+  f04443: 'candidate, China, Shanghai, 罗良科技',
+  f05315: 'candidate, USA, Portland, 6block',
   f05664: 'candidate, China, Shenzhen, 艾比特网络科技',
-  f07819: 'candidate, China, Wuxi, YC--wuxi',
   f07824: 'candidate, China, Shanghai',
   f07850: 'candidate, USA, Portland, 6block',
   f07919: 'candidate, China, Beijing, Shanghai, Hunan',
   f07990: 'candidate, China, Hong Kong',
   f07998: 'candidate, China, Hangzhou',
-  f08019: 'candidate, China, Yantai, 三合',
   f08025: 'candidate, China, Hong Kong',
   f08091: 'candidate, USA',
-  f08157: 'candidate, China, Beijing + Germany, Frankfurt, 超星际',
   f08220: 'candidate, China, Shenzhen',
-  f08240: 'candidate, Russia, Novosibirsk, Rabinovitch',
   f08264: 'candidate, China, Xianning',
-  f08403: 'candidate, UK, Lower Slaughter',
-  f08474: 'candidate, China, Hong Kong',
+  f08371: 'candidate, China, Chengdu',
+  f08383: 'candidate, USA, Portland, 6block',
   f09573: 'candidate, China, Xiamen',
   f09589: 'candidate, China, Changsha',
-  f09696: 'candidate, USA, Portland + Singapore',
-  f09770: "candidate, China, Xi'an",
+  f09833: 'candidate, USA, San Jose',
   f09848: 'candidate, USA',
   f010035: 'candidate, Netherlands',
-  f010247: 'candidate, China, Yantai',
+  f010063: 'candidate, China, Mianyang',
+  f010241: 'candidate, China, Beijing + USA, Portland',
+  f010253: 'candidate, China, Beijing + USA, Portland',
   f010399: 'candidate, China, Guangzhou',
-  f010400: 'candidate, China, Shenyang',
-  f010446: 'candidate, Netherlands',
-  f010479: 'candidate, France, Fontenay-sous-Bois',
-  f010491: 'candidate, China, Shenzhen',
   f010493: 'candidate, China, Shanghai',
-  f010498: 'candidate, China, Shenzhen',
   f010501: 'candidate, China, Hangzhou',
   f010505: 'candidate, China, Wuhan',
-  f010507: 'candidate, China, Hangzhou',
   f010513: 'candidate, China, Chongqing',
   f010528: 'candidate, China, Chongqing',
-  f014324: 'candidate, Canada, Vancouver',
+  f010558: 'candidate, China, Shenzhen + Hong Kong',
   f014394: 'candidate, China, Suzhou',
   f014569: 'candidate, NR',
-  f014768: 'candidate, Singapore',
   f015877: 'candidate, China, Jiaxing + USA',
-  f015927: 'candidate, USA, East Islip',
   f015941: 'candidate, Ukraine, Kyiv',
   f018501: 'candidate, China, Beijing',
   f018780: 'candidate, Singapore + Germany, Frankfurt + USA',
   f018781: 'candidate, Singapore + Germany, Frankfurt + USA',
   f018782: 'candidate, Singapore + Germany, Frankfurt + USA',
+  f018783: 'candidate, Singapore + Germany, Frankfurt + USA',
   f018784: 'candidate, Singapore + Germany, Frankfurt + USA',
-  f019029: 'candidate, Serbia, Boljevac',
-  f019100: 'candidate, Romania, Cluj-Napoca',
+  f018785: 'candidate, Singapore + Germany, Frankfurt + USA',
   f019240: 'candidate, China, Beijing',
-  f019551: 'candidate, UK',
+  f019354: 'candidate, Germany, Frankfurt',
   f019638: 'candidate, Germany, Frankfurt',
   f019661: 'candidate, Russia, Pyatigorsk',
   f020489: 'candidate, China, Lioaning',
   f020523: 'candidate, China, Suzhou',
-  f020541: 'candidate, China, Shanghai',
   f020608: 'candidate, Russia',
   f020904: 'candidate, Korea, Uijeongbu-si',
-  f021255: 'candidate, China, Suzhou + Netherlands',
+  f020928: 'candidate, China, Deyang + Singapore',
   f021262: 'candidate, China, Shanghai',
   f021265: 'candidate, China, Wuhan',
   f021444: 'candidate, China, Guangzhou',
   f021532: 'candidate, China, Shanghai',
-  f021540: 'candidate, Germany, Frankfurt, @hsanjuan',
-  f021870: 'candidate, USA, Portland',
-  f022072: 'candidate, China, Jieyang',
-  f022142: 'candidate, USA, Queens',
-  f022163: 'candidate, NR',
-  f022227: 'candidate, China, Jiangsu',
-  f022352: 'candidate, Norway, Borgen',
   f022522: 'candidate, China, Dongguan',
   f022853: 'candidate, China, Fuzhou',
-  f023492: 'candidate, China, Beijing',
-  f023495: 'candidate, China, Beijing + USA, Portland',
   f023565: 'candidate, China, Fuzhou',
-  f023647: 'candidate, Korea, Gangseo-gu',
+  f023581: 'candidate, China, Hong Kong',
   f023649: 'candidate, Korea, Seoul',
   f023660: 'candidate, China, Fuzhou',
   f023662: 'candidate, China, Fuzhou',
   f023825: 'candidate, USA, Portland',
-  f023854: 'candidate, undefined',
   f023855: 'candidate, China, Fuzhou',
   f023858: 'candidate, China, Fuzhou',
   f023870: 'candidate, China, Fuzhou',
   f023871: 'candidate, China, Fuzhou',
   f023928: 'candidate, China, Fuzhou',
   f023939: 'candidate, NR',
-  f023971: 'candidate, USA, @Brian Y',
   f024006: 'candidate, China, Fuzhou',
-  f024012: 'candidate, China, Fuzhou',
-  f024013: 'candidate, China, Fuzhou',
   f024014: 'candidate, China, Fuzhou',
+  f024015: 'candidate, USA, Portland',
   f024025: 'candidate, China, Chengdu',
-  f024030: 'candidate, Korea, Gwangju',
-  f024066: 'candidate, NR',
-  f024081: 'candidate, China, Fuzhou',
-  f024084: 'candidate, China, Fuzhou',
-  f024085: 'candidate, China, Fuzhou',
   f024127: 'candidate, China, Fuzhou',
   f024136: 'candidate, China, Fuzhou',
   f024146: 'candidate, China, Fuzhou',
@@ -158,79 +117,108 @@ const annotations = {
   f024148: 'candidate, China, Fuzhou',
   f024156: 'candidate, USA',
   f024468: 'candidate, China, Shenzhen',
-  f024800: 'candidate, China, Hong Kong',
-  f024969: 'candidate, NR',
-  f025017: 'candidate, China, Hefei',
-  f025019: 'candidate, China, Taiwan, Hsinchu',
-  f029368: 'candidate, Korea, Gangseo-gu',
   f029377: 'candidate, China, Suzhou',
+  f029401: 'candidate, USA, Portland + New Castle',
   f029404: 'candidate, USA, Portland + New Castle',
-  f029490: 'candidate, NR',
-  f029524: 'candidate, China, Hong Kong',
   f029580: 'candidate, China, Maoming',
+  f030125: 'candidate, China, Nantong',
+  f030184: 'candidate, USA, Aliquippa',
   f030226: 'candidate, China, Fuzhou',
   f030230: 'candidate, China, Sichuan',
-  f030335: 'candidate, China, Fujian',
-  f030379: 'candidate, Korea, Uiwang',
   f030391: 'candidate, China, Chengdu',
-  f032835: 'candidate, Japan, Setagaya-ku',
   f032913: 'candidate, China, Shenzhen',
   f032977: 'candidate, China, Deyang',
   f033048: 'candidate, USA, Ashburn',
   f033091: 'candidate, China, Wuxi',
+  f033111: 'candidate, China, Shanghai',
   f033113: 'candidate, China, Wuxi',
+  f033124: 'candidate, China, Shanghai',
+  f033206: 'candidate, Canada',
+  f033438: 'candidate, USA',
+  f033517: 'candidate, China, Changsha',
   f033557: 'candidate, China, Hong Kong',
 
   // Testing
 
   // Active
 
-  f01234: 'candidate, Belgium, De Klinge, Eliovp',
-  f01240: 'candidate, Netherlands, HidNand',
-  f01279: 'candidate, China, Sichuan',
-  f02299: 'candidate, China, Dongguan, Yolo',
-  f02381: 'candidate, China, Dongguan, 西野七濑迷叔',
-  f02405: 'candidate, China, Shenzhen',
-  f02419: 'candidate, China, Shanghai, yuantai',
-  f02423: 'candidate, Ukraine, Kyiv, igowreck',
-  f02490: 'candidate, China, Fujian',
-  f02625: 'candidate, China, Shanghai, IPFS星际工厂',
-  f03194: 'candidate, China, Shenzhen, 成都云存&Npool',
-  f03224: 'candidate, Ukraine, Irpin, oboltusov',
-  f03339: 'candidate, China, Luzhou, benxun',
-  f03491: 'candidate, China, Hong Kong',
-  f05664: 'candidate, China, Shenzhen, 艾比特网络科技',
-  f07919: 'candidate, China, Beijing, Shanghai, Hunan',
-  f07990: 'candidate, China, Hong Kong',
-  f09589: 'candidate, China, Changsha',
-  f09848: 'candidate, USA',
-  f015941: 'candidate, Ukraine, Kyiv',
-  f018781: 'candidate, Singapore + Germany, Frankfurt + USA',
-  f018782: 'candidate, Singapore + Germany, Frankfurt + USA',
-  f019661: 'candidate, Russia, Pyatigorsk',
-  f020489: 'candidate, China, Lioaning',
-  f020608: 'candidate, Russia',
-  f020904: 'candidate, Korea, Uijeongbu-si',
-  f021262: 'candidate, China, Shanghai',
-  f021532: 'candidate, China, Shanghai',
-  f023649: 'candidate, Korea, Seoul',
-  f023660: 'candidate, China, Fuzhou',
-  f023662: 'candidate, China, Fuzhou',
-  f023858: 'candidate, China, Fuzhou',
-  f023871: 'candidate, China, Fuzhou',
-  f023939: 'candidate, NR',
-  f024014: 'candidate, China, Fuzhou',
-  f024127: 'candidate, China, Fuzhou',
-  f024136: 'candidate, China, Fuzhou',
-  f024146: 'candidate, China, Fuzhou',
-  f029377: 'candidate, China, Suzhou',
-  f029404: 'candidate, USA, Portland + New Castle',
-  f029580: 'candidate, China, Maoming',
-  f030230: 'candidate, China, Sichuan',
-  f030391: 'candidate, China, Chengdu',
-  f032977: 'candidate, China, Deyang',
-  f033048: 'candidate, USA, Ashburn',
-  f033091: 'candidate, China, Wuxi',
+  f01238: 'active, Vietnam, Hanoi, FILECOIN-VIETNAM', // 638027
+  f01247: 'active, Canada, Montreal, BigChungus™', // 638475
+  f01272: 'active, Singapore', // 637896
+  f01278: 'active, USA, Grand Rapids, MiMiner', // 637943
+  f01782: 'active, China, Hong Kong + Singapore + USA, Portland, hellofil.com', // 636646
+  f02301: 'active, USA', // 637950
+  f02401: 'active, Canada, Chambly, NBFS Canada', // 637974
+  f02416: 'active, China, Guangdong, 星际无限', // 640153
+  f02620: 'active, Poland, Krakow, @magik6k', // 638472
+  f02645: 'active, China, Hong Kong', // 672439
+  f03176: 'active, USA, San Mateo, Five Star-Helmsman&Heiben', // 638488
+  f03273: 'active, USA, Portland, 6block', // 638477
+  f03274: 'active, USA, Portland, 6block', // 638468
+  f03275: 'active, USA, Portland, 6block', // 639959
+  f03302: 'active, China, Chengdu', // 639977
+  f03363: 'active, Singapore', // 637963
+  f03488: 'active, Korea, Geumcheon-gu', // 607595
+  f05316: 'active, USA, Portland, 6block', // 637980
+  f05317: 'active, USA, Portland', // 638481
+  f07819: 'active, China, Wuxi, YC--wuxi', // 637947
+  f08019: 'active, China, Yantai, 三合', // 636652
+  f08094: 'active, China, Guangzhou, dongshao', // 639950
+  f08157: 'active, China, Beijing + Germany, Frankfurt, 超星际', // 638483
+  f08240: 'active, Russia, Novosibirsk, Rabinovitch', // 638494
+  f08403: 'active, UK, Lower Slaughter', // 638003
+  f08474: 'active, China, Hong Kong', // 674230
+  f09696: 'active, USA, Portland + Singapore', // 637892
+  f09731: 'active, China, Guiyang', // 637872
+  f09770: "active, China, Xi'an", // 665564
+  f010088: 'active, DNS:purumine.com', // 638482
+  f010247: 'active, China, Yantai', // 636657
+  f010400: 'active, China, Shenyang', // 674081
+  f010446: 'active, Netherlands', // 638591
+  f010479: 'active, France, Fontenay-sous-Bois', // 674084
+  f010491: 'active, China, Shenzhen', // 640003
+  f010498: 'active, China, Shenzhen', // 640173
+  f010507: 'active, China, Hangzhou', // 638486
+  f014768: 'active, Singapore', // 638484
+  f015927: 'active, USA, East Islip', // 610534
+  f019029: 'active, Serbia, Boljevac', // 675244
+  f019551: 'active, UK', // 636790
+  f020436: 'active, China, Beijing', // 636724
+  f020541: 'active, China, Shanghai', // 640111
+  f021255: 'active, China, Suzhou + Netherlands', // 638037
+  f021540: 'active, Germany, Frankfurt, @hsanjuan', // 638555
+  f021870: 'active, USA, Portland', // 638471
+  f022072: 'active, China, Jieyang', // 675136
+  f022142: 'active, USA, Queens', // 638480
+  f022227: 'active, China, Jiangsu', // 640662
+  f022352: 'active, Norway, Borgen', // 674522 3 minutes
+  f022922: 'active, China, Shenzhen', // 639958
+  f023152: 'active, China, Quanzhou', // 636787
+  f023492: 'active, China, Beijing', // 676869
+  f023495: 'active, China, Beijing + USA, Portland', // 638469
+  f023647: 'active, Korea, Gangseo-gu', // 674245
+  f023854: 'active, China, Fuzhou', // 672627
+  f023971: 'active, USA, @Brian Y', // 637125
+  f024012: 'active, China, Fuzhou',
+  f024013: 'active, China, Fuzhou',
+  f024030: 'active, Korea, Gwangju', // 637137
+  f024066: 'active, NR', // 639846
+  f024081: 'active, China, Fuzhou',
+  f024084: 'active, China, Fuzhou',
+  f024085: 'active, China, Fuzhou',
+  f024800: 'active, China, Hong Kong', // 640042
+  f024969: 'active, NR', // 640063
+  f025017: 'active, China, Hefei', // 638552
+  f025019: 'active, China, Taiwan, Hsinchu', // 637930
+  f029368: 'active, Korea, Gangseo-gu', // 672639
+  f029490: 'active, NR', // 675184
+  f029524: 'active, China, Hong Kong', // 672651
+  f029556: 'active, China, Wuxi', // 636729
+  f029566: 'active, NR', // 640058
+  f030296: 'active, USA, Cary', // 636964
+  f030379: 'active, Korea, Uiwang', // 637023
+  f032835: 'active, Japan, Setagaya-ku', // 675751
+  f032934: 'active, Korea, Incheon', // 674920
 
   // Active/sealing
 
@@ -240,7 +228,8 @@ const annotations = {
   f01800: 'sealing, China, Shenzhen + Chengdu', // 637852
   f02388: 'sealing, Netherlands, Amersfoort, Kroketje', // 637890
   f02492: 'sealing, China, Shenzhen', // 640361
-  f02633: 'sealing, China, Hong Kong', // 706745
+  f02622: 'sealing, USA, Portland, 6block', // 610403
+  f02623: 'sealing, USA, Portland, 6block', // 610920
   f02665: 'sealing, Netherlands, Amsterdam, fm-ops', // 640077
   f03223: 'sealing, USA', // 637913
   f03264: 'sealing, Korea, Bucheon-si', // 672827
@@ -249,28 +238,22 @@ const annotations = {
   f03482: 'sealing, China, Wuxi', // 680177
   f03512: 'sealing, New Zealand, Auckland, NZFILECOIN01', // 679779
   f03624: 'sealing, Germany, Chemnitz, ode', // 675480
-  f04443: 'sealing, China, Shanghai, 罗良科技', // 705857
   f07806: 'sealing, China, Wuhan, HelloXP', // 672730
   f08204: 'sealing, USA, St. Louis', // 684022
   f08285: 'sealing, Latvia, Riga, stander', // 636429
-  f08383: 'sealing, USA, Portland, 6block', // 706565
   f08482: 'sealing, USA, Athol, @why', // 675595
-  f09693: 'sealing, USA, Portland + Singapore',
-  f09833: 'sealing, USA, San Jose', // 706035
-  f010241: 'sealing, China, Beijing + USA, Portland', // 705706
-  f010253: 'sealing, China, Beijing + USA, Portland', // 706878
+  f09693: 'sealing, USA, Portland + Singapore', // 607276
   f010512: 'sealing, China, Zhengzhou', // 675591
-  f010558: 'sealing, China, Shenzhen + Hong Kong', // 706835
   f014281: 'sealing, Netherlands, Permerend', // 683833
+  f014324: 'sealing, Canada, Vancouver', // 636892
   f014409: 'sealing, USA, Waxhaw', // 674544
   f015734: 'sealing, China, Qingdao', // 636961
   f016594: 'sealing, Germany', // 637865
-  f018783: 'sealing, Singapore + Germany, Frankfurt + USA', // 705886
+  f019100: 'sealing, Romania, Cluj-Napoca', // 638470
   f019104: 'sealing, Canada, Chambly', // 640046
   f019118: 'sealing, China, Jiaxing', // 641067
   f019243: 'sealing, USA, Jackson', // 640152
   f019362: 'sealing, China, Hangzhou', // 636647
-  f020928: 'sealing, China, Deyang + Singapore', // 706738
   f021274: 'sealing, China, Shanghai', // 636678
   f021339: 'sealing, China, Jiaxing', // 636422
   f021346: 'sealing, China, Shanghai', // 636865
@@ -278,6 +261,7 @@ const annotations = {
   f021583: 'sealing, China, Jiaxing', // 637878
   f021710: 'sealing, China, Shijiazhuang', // 637997
   f022125: 'sealing, China, Shenzhen', // 677161
+  f022163: 'sealing, NR', // 675533
   f022376: 'sealing, China, Dongguan', // 681287
   f022395: 'sealing, USA, Los Angeles', // 672467
   f022753: 'sealing, China, Hong Kong', // 673939
@@ -295,8 +279,7 @@ const annotations = {
   f023981: 'sealing, China, Fuzhou',
   f024007: 'sealing, China, Fuzhou',
   f024008: 'sealing, China, Fuzhou',
-  f024015: 'sealing, USA, Portland', // 706411
-  f024016: 'sealing, USA, Portland',
+  f024016: 'sealing, USA, Portland', // 607599
   f024130: 'sealing, Korea', // 637065
   f024168: 'sealing, USA, Ashburn', // 675716
   f024550: 'sealing, China, Wenzhou', // 679823
@@ -305,45 +288,24 @@ const annotations = {
   f029529: 'sealing, China, Dongguan', // 673975
   f029587: 'sealing, Hungary', // 636827
   f030144: 'sealing, Korea, Gyeonggi-do', // 674425
+  f030335: 'sealing, China, Fujian', // 636939
+  f030338: 'sealing, USA, Santa Fe Springs', // 637119
   f030518: 'sealing, China, Wuhan', // 637121
   f030641: 'sealing, China, Shaanxi', // 636870
   f032865: 'sealing, USA, Redmond', // 672706
   f032951: 'sealing, Korea, Gyeonggi-do', // 675392
   f032976: 'sealing, China, Taiwan, Taipei', // 676210
-  f033028: 'sealing, China, Mianyang', // 674716
-  f033111: 'sealing, China, Shanghai', // 707071
-  f033124: 'sealing, China, Shanghai', // 705832
-  f033517: 'sealing, China, Changsha', // 707008
+  f033028: 'sealing, China, Mianyang' // 674716
 
   // Stuck
 
-  f01241: 'stuck, USA, Portland, 6block', // ProposalAccepted: 36 minutes
-  f02501: 'stuck, USA, Portland, 6block', // ProposalAccepted: 36 minutes
-  f02514: 'stuck, USA, Portland, 6block', // ProposalAccepted: 30 minutes
-  f05315: 'stuck, USA, Portland, 6block', // ProposalAccepted: 27 minutes
-  f09639: 'stuck, undefined', // CheckForAcceptance: about 4 hours
-  f010063: 'stuck, China, Mianyang', // Transferring: 28 minutes
-  f018785: 'stuck, Singapore + Germany, Frankfurt + USA', // ProposalAccepted: 26 minutes
-  f019354: 'stuck, Germany, Frankfurt', // ProposalAccepted: 13 minutes
-  f023581: 'stuck, China, Hong Kong', // CheckForAcceptance: 32 minutes
-  f029401: 'stuck, USA, Portland + New Castle', // ProposalAccepted: 23 minutes
-  f030125: 'stuck, China, Nantong', // Transferring: 32 minutes
-  f030184: 'stuck, USA, Aliquippa', // ProposalAccepted: 28 minutes
-  f030327: 'stuck, undefined', // ProposalAccepted: about 4 hours
-  f033206: 'stuck, Canada', // ProposalAccepted: 3 minutes
-
   // Min-size
-
-  f02576: 'min-size, Denmark, Gjerlev, BenjaminH', // 134217728 < 1073741824
 
   // Min-ask
 
   // Error
 
   // Rejected
-
-  f08371: 'rejected, China, Chengdu', // piece size more than maximum allowed size: 134217728 > 1048576
-  f033438: 'rejected, USA' // piece size more than maximum allowed size: 134217728 > 1048576
 
   // Dial
 
