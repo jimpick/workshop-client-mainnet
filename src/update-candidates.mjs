3ae7691d1a85885a332de22602dc896a165704ec
Jim Pick <jim@jimpick.com>
2020-11-27 22:48:05 -0800

    New candidates

--- src/update-candidates.mjs
+++ src/update-candidates.mjs
@@ -1,4 +1,3 @@
-
 import annotations from './annotations-spacerace.js'
 import annotations2 from './annotations-spacerace-slingshot-medium.js'
 import annotations3 from './annotations-spacerace-slingshot-medium-jim.js'
@@ -17,12 +16,19 @@ const newEntries = []
 for (const miner in annotations) {
   const annotation = annotations[miner]
   if (!newAnnotations[miner]) {
-    if (annotation.match(/^active/) ||
-        annotation.match(/^sealing/) ||
-        annotation.match(/^busy/) ||
-        annotation.match(/^min-/)) {
+    if (
+      annotation.match(/^active/) ||
+      annotation.match(/^sealing/) ||
+      annotation.match(/^stuck/) ||
+      annotation.match(/^busy/) ||
+      annotation.match(/^min-/)
+    ) {
       let newTag = 'candidate,'
-      if (annotations3[miner] && annotations3[miner].match(/active,/)) {
+      if (
+        annotations3[miner] &&
+        (annotations3[miner].match(/active,/) ||
+          annotations3[miner].match(/active-sealing,/))
+      ) {
         newTag = 'active-candidate,'
       }
       const newEntry = annotation.replace(/^[^,]*,/, newTag)
@@ -35,5 +41,8 @@ newEntries.sort(([a], [b]) => {
   return Number(a.slice(1)) - Number(b.slice(1))
 })
 
-console.log(newEntries.map(entry => `  ${entry[0]}: ${JSON.stringify(entry[1])},`).join('\n'))
-
+console.log(
+  newEntries
+    .map(entry => `  ${entry[0]}: ${JSON.stringify(entry[1])},`)
+    .join('\n')
+)
