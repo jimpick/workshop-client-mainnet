8fadd5e368a20c37d251bb0207b560dd944dcf01
Jim Pick <jim@jimpick.com>
2021-05-25 13:12:38 -0700

    Publish annotated-miner-indexes.json with list of miner indexes with annotations

---
+++ src/make-json.js
@@ -0,0 +1,22 @@
+/*
+import fs from 'fs'
+import annotations from './annotations-mainnet.js'
+import annotations128mibUnverified from './annotations-mainnet-128mib-unverified.js'
+import annotations128mibVerified from './annotations-mainnet-128mib-verified.js'
+
+fs.writeFileSync('annotations-mainnet.json', JSON.stringify(annotations, null, 2))
+fs.writeFileSync('annotations-mainnet-128mib-unverified.json', JSON.stringify(annotations128mibUnverified, null, 2))
+fs.writeFileSync('annotations-mainnet-128mib-verified.json', JSON.stringify(annotations128mibVerified, null, 2))
+*/
+
+const fs = require('fs')
+
+const annotationsText = fs.readFileSync('./annotations-mainnet.js', 'utf8')
+const modified = annotationsText.replace(/export.*/, '').replace('const annotations = ', 'x = ')
+// console.log(modified)
+const annotations = eval(modified)
+const minerIndexes = Object.keys(annotations)
+  .map(miner => Number(miner.slice(2)))
+  .sort((a, b) => a - b)
+fs.writeFileSync('./annotated-miner-indexes.json', JSON.stringify(minerIndexes, null, 2))
+fs.writeFileSync('./annotations-mainnet.json', JSON.stringify(annotations, null, 2))
--- src/make-json.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-import annotations128mibUnverified from './annotations-mainnet-128mib-unverified.js'
-import annotations128mibVerified from './annotations-mainnet-128mib-verified.js'
-
-fs.writeFileSync('annotations-mainnet-128mib-unverified.json', JSON.stringify(annotations128mibUnverified, null, 2))
-fs.writeFileSync('annotations-mainnet-128mib-verified.json', JSON.stringify(annotations128mibVerified, null, 2))
